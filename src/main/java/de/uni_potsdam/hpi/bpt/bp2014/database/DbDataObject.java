f69c475052c5c146b28517c51dc2dca6ef4716e6
Jaspar Mang <jaspar.mang@student.hpi.uni-potsdam.de>
2015-01-05 16:18:45 +0100

    implemented data object creation

--- src/main/java/de/uni_potsdam/hpi/bpt/bp2014/database/DbDataObject.java
+++ src/main/java/de/uni_potsdam/hpi/bpt/bp2014/database/DbDataObject.java
@@ -9,21 +9,58 @@ import java.util.LinkedList;
  * Created by jaspar.mang on 05.01.15.
  */
 public class DbDataObject {
-    public LinkedList<String> getDataObjectsForScenario(int scenario_id) {
+    public LinkedList<Integer> getDataObjectsForScenario(int scenario_id) {
         java.sql.Connection conn = Connection.getInstance().connect();
         Statement stmt = null;
         ResultSet rs = null;
-        LinkedList<String> results = new LinkedList<String>();
+        LinkedList<Integer> results = new LinkedList<Integer>();
         if (conn == null) return results;
 
         try {
             //Execute a query
             stmt = conn.createStatement();
-            String sql = "SELECT name FROM datanode WHERE scenario_id = " + scenario_id + " GROUP BY name ORDER BY dataclass_id";
+            String sql = "SELECT id FROM dataobject WHERE scenario_id = " + scenario_id;
             rs = stmt.executeQuery(sql);
             while(rs.next()){
-                results.add(rs.getString("name"));
+                results.add(rs.getInt("id"));
             }
+            //Clean-up environment
+            rs.close();
+            stmt.close();
+            conn.close();
+        } catch (SQLException se) {
+            //Handle errors for JDBC
+            se.printStackTrace();
+        } finally {
+            //finally block used to close resources
+            try {
+                if (stmt != null)
+                    stmt.close();
+            } catch (SQLException se2) {
+            }// nothing we can do
+            try {
+                if (conn != null)
+                    conn.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+        return results;
+    }
+    public int getStartStateID(int dataObject_id) {
+        java.sql.Connection conn = Connection.getInstance().connect();
+        Statement stmt = null;
+        ResultSet rs = null;
+        int results = -1;
+        if (conn == null) return results;
+
+        try {
+            //Execute a query
+            stmt = conn.createStatement();
+            String sql = "SELECT start_state_id FROM dataobject WHERE id = " + dataObject_id;
+            rs = stmt.executeQuery(sql);
+            rs.next();
+            results = rs.getInt("start_state_id");
             //Clean-up environment
             rs.close();
             stmt.close();
