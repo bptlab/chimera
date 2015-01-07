73dbae9ee99f52b0e0e990b761a0f3d256ed2940
jaSunny <jannik@github.com>
2015-01-07 16:18:59 +0100

    JP-42 #comments: more additions

--- samples/FrontendMockup/v2/forms.php
+++ samples/FrontendMockup/v2/forms.php
@@ -114,25 +114,6 @@
                 </div>
             <div class="widget-body">
 
-                <!-- Expand and collapse a div using javascript -->    
-                <script>
-                $(".collapse_custom").click(function () {
-                      $header = $(this);
-                      //getting the next element
-                      $content = $header.next();
-                      //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
-                      $content.slideToggle(500, function () {
-                          //execute this after slideToggle is done
-                          //change text of header based on visibility of content div
-                          $header.text(function () {
-                              //change text based on condition
-                              return $content.is(":visible") ? "Collapse" : "Expand";
-                          });
-                      });
-
-                  });
-                </script>
-
                 <div class="container_custom">
                     <div class="collapse_custom"><span>Expand</span>
                     </div>
--- samples/FrontendMockup/v2/header.php
+++ samples/FrontendMockup/v2/header.php
@@ -52,6 +52,25 @@
     }
     </style>
 
+        <!-- Expand and collapse a div using javascript -->    
+    <script language="javascript" type="text/javascript">
+    $(".collapse_custom").click(function () {
+          $header = $(this);
+          //getting the next element
+          $content = $header.next();
+          //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
+          $content.slideToggle(500, function () {
+              //execute this after slideToggle is done
+              //change text of header based on visibility of content div
+              $header.text(function () {
+                  //change text based on condition
+                  return $content.is(":visible") ? "Collapse" : "Expand";
+              });
+          });
+
+      });
+    </script>
+
   </head>
   <body>
     <header>
