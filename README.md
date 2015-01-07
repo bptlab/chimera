6d4e67b25ab8354c1721b81db88bcfe8316c7adf
sihde <sven.ihde@student.hpi.de>
2015-01-07 15:55:55 +0100

    now it is only possible to return a json-serialised list of enabled activities

--- README.md
+++ README.md
@@ -4,6 +4,12 @@
 
 The JEngine is a Framework for Production Case Management (PCM) containing Modeling and Execution.
 
+## RESTful
+
+To test the rest-based implementation the RestTest-Class exists to create the server, which shall be later exchanged
+with the main class that executes the main method and the server.
+As of now the server will give you an response on localhost:8080/rest and will give you an json.
+
 ## Features
 
 This JEngine does execute
--- src/main/java/de/uni_potsdam/hpi/bpt/bp2014/jcore/RestConnection.java
+++ src/main/java/de/uni_potsdam/hpi/bpt/bp2014/jcore/RestConnection.java
@@ -11,13 +11,10 @@ import java.util.LinkedList;
 public class RestConnection {
     @GET
     @Path( "Scenarioname/{Scenarioname}" )
-    @Produces(MediaType.TEXT_XML)
-    public void executeScenario( @PathParam("Scenarioname") ScenarioInstance scenarioInstance){
+    @Produces(MediaType.APPLICATION_JSON)
+    public LinkedList showEnabledActivities( @PathParam("Scenarioname") ScenarioInstance scenarioInstance){
         ExecutionService executionService = new ExecutionService(scenarioInstance);
         LinkedList<Integer> enabledActivitiesIDs= executionService.getEnabledActivitiesIDs();
-        System.out.println("enabled Aktivität ID");
-        for(int activityID: enabledActivitiesIDs){
-            System.out.println(activityID);
-        }
+        return enabledActivitiesIDs;
     }
 }
