9495a8271bef7c51545ea113ff1b353dffface78
Jaspar Mang <jaspar.mang@student.hpi.uni-potsdam.de>
2015-01-12 10:37:36 +0100

    added Tests for DbScenario, DbScenarioInstance, DbActivityInstance

---
+++ src/test/java/de/uni_potsdam/hpi/bpt/bp2014/DbActivityInstanceTest.java
@@ -0,0 +1,24 @@
+package de.uni_potsdam.hpi.bpt.bp2014;
+
+import static org.junit.Assert.*;
+
+import de.uni_potsdam.hpi.bpt.bp2014.database.DbActivityInstance;
+import org.junit.Test;
+
+/**
+ * Created by jaspar.mang on 12.01.15.
+ */
+public class DbActivityInstanceTest {
+
+    @Test
+    public void testGetState(){
+        DbActivityInstance dbActivityInstance = new DbActivityInstance();
+        assertEquals("ready", dbActivityInstance.getState(77));
+    }
+    @Test
+    public void testSetState() {
+        DbActivityInstance dbActivityInstance = new DbActivityInstance();
+        dbActivityInstance.setState(91, "terminated");
+        assertEquals("terminated", dbActivityInstance.getState(88));
+    }
+}
---
+++ src/test/java/de/uni_potsdam/hpi/bpt/bp2014/DbScenarioInstanceTest.java
@@ -0,0 +1,45 @@
+package de.uni_potsdam.hpi.bpt.bp2014;
+
+
+import de.uni_potsdam.hpi.bpt.bp2014.database.DbScenarioInstance;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import java.util.LinkedList;
+
+/**
+ * Created by jaspar.mang on 12.01.15.
+ */
+public class DbScenarioInstanceTest {
+    @Test
+    public void testExistScenario(){
+        DbScenarioInstance dbScenarioInstance = new DbScenarioInstance();
+        assertTrue(dbScenarioInstance.existScenario(50));
+        assertFalse(dbScenarioInstance.existScenario(999));
+        assertTrue(dbScenarioInstance.existScenario(1, 50));
+        assertFalse(dbScenarioInstance.existScenario(1, 999));
+    }
+
+    @Test
+    public void testGetScenarioInstanceID(){
+        DbScenarioInstance dbScenarioInstance = new DbScenarioInstance();
+        assertEquals(86, dbScenarioInstance.getScenarioInstanceID(100));
+    }
+
+    @Test
+    public void testGetScenarioInstances(){
+        DbScenarioInstance dbScenarioInstance = new DbScenarioInstance();
+        LinkedList<Integer> instances = dbScenarioInstance.getScenarioInstances(100);
+        assertEquals(85, (int)instances.get(0));
+        assertEquals(86, (int)instances.get(1));
+    }
+
+    @Test
+    public void testCreateNewScenarioInstance(){
+        DbScenarioInstance dbScenarioInstance = new DbScenarioInstance();
+        dbScenarioInstance.createNewScenarioInstance(101);
+        assertNotNull(dbScenarioInstance.getScenarioInstanceID(101));
+    }
+
+
+
+}
