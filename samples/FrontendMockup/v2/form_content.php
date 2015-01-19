a4439aa5998ddf1469ce04e8132a969290cd2b71
jaSunny <jannik@github.com>
2015-01-19 11:21:33 +0100

    adding REST call for open tasks

--- samples/FrontendMockup/v2/form_content.php
+++ samples/FrontendMockup/v2/form_content.php
@@ -1,4 +1,16 @@
+<?php
+
+$PCM_Scenario = "";
+$PCM_Fragment = "";
+
+$get_json = file_get_contents($JEngine_Server_URL."/".$JCore_REST_Interface."/".$PCM_Scenario."/".$PCM_Fragment);
+$get_response = json_decode($get_json,true);
+
+//echo  $get_response['John'][status];
+?>
+
                         <li>Teil kleben</li>
                         <li>Teil transportieren</li>
                         <li>Teil schweißen</li>
                         <li><font color="green">Teil schrauben</font></li>
+
--- samples/FrontendMockup/v2/header.php
+++ samples/FrontendMockup/v2/header.php
@@ -1,3 +1,7 @@
+<?php
+include 'config.php';
+?>
+
 <!DOCTYPE html>
 <!--[if lt IE 7]>
 
