058b57d898b8a356ef4c62f6b495d503736e5732
jaSunny <jannik@github.com>
2015-01-19 11:21:33 +0100

    adding REST call for open tasks

---
+++ samples/FrontendMockup/v2/config.php
@@ -0,0 +1,11 @@
+<?php
+
+###################################################
+#
+#	JEngine Server URL
+$JEngine_Server_URL = "http://172.16.64.113" ;
+
+###################################################
+#
+#	REST Interface of the JEngine
+$JCore_REST_Interface = "JEngine/Scenario" ;
