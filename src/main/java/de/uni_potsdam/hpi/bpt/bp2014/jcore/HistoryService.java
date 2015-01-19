d6f7d354d52588eda6182e7330d3b56246f59ade
Jaspar Mang <jaspar.mang@student.hpi.uni-potsdam.de>
2015-01-19 13:07:18 +0100

    added activity labels in history service

--- src/main/java/de/uni_potsdam/hpi/bpt/bp2014/jcore/HistoryService.java
+++ src/main/java/de/uni_potsdam/hpi/bpt/bp2014/jcore/HistoryService.java
@@ -1,7 +1,9 @@
 package de.uni_potsdam.hpi.bpt.bp2014.jcore;
 
 import de.uni_potsdam.hpi.bpt.bp2014.database.DbActivityInstance;
+import de.uni_potsdam.hpi.bpt.bp2014.database.DbControlNode;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 
 /**
@@ -9,9 +11,18 @@ import java.util.LinkedList;
  */
 public class HistoryService {
     private DbActivityInstance dbActivityInstance = new DbActivityInstance();
+    private DbControlNode dbControlNode = new DbControlNode();
 
     public LinkedList<Integer> getTerminatedActivitysForScenarioInstance(int scenarioInstance_id){
         LinkedList<Integer> ids = dbActivityInstance.getTerminatedActivitiesForScenarioInstance(scenarioInstance_id);
         return  ids;
+    }
+    public HashMap<Integer, String> getTerminatedActivityLabelsForScenarioInstance(int scenarioInstance_id){
+        LinkedList<Integer> ids = dbActivityInstance.getTerminatedActivitiesForScenarioInstance(scenarioInstance_id);
+        HashMap<Integer, String> labels = new HashMap<Integer, String>();
+        for (int id: ids){
+            labels.put(id, dbControlNode.getLabel(id));
+        }
+        return  labels;
     }
 }
