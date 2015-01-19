af068a35b217ad893798747a42562a0a2887c2cc
jaSunny <jannik@github.com>
2015-01-19 17:27:01 +0100

    smaller changes

--- samples/FrontendMockup/v2/form_content.php
+++ samples/FrontendMockup/v2/form_content.php
@@ -1,4 +1,5 @@
 <?php
+include 'config.php';
 
 $PCM_Scenario = "1";
 $PCM_Fragment = "1";
@@ -6,16 +7,15 @@ $PCM_options = "closed";
 
 $get_json = file_get_contents($JEngine_Server_URL."/".$JCore_REST_Interface."/".$PCM_Scenario."/".$PCM_Fragment."/".$PCM_options);
 
-//$get_json = '{"ids":[2,4],"label":{"2":"Essen kochen","4":"Zutaten kaufen"}}';
+//Exmaple JSON String: '{"ids":[2,4],"label":{"2":"Essen kochen","4":"Zutaten kaufen"}}';
 $get_response = json_decode($get_json,true);
 
-echo "get_json: ".$get_json;
-echo $get_response['ids'];
-print_r($get_response['label']);
-echo "get_response: "; var_dump($get_response);
+$response_amount_of_ids = count($get_response['ids']);
+$response_label = $get_response['label'];
 
-print_r(json_last_error());
-
+if($response_amount_of_ids == 0){
+	die (" no ids provided");
+}
 
 ?>
 
