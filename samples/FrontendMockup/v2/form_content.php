735b4aa4b498e0ba87fe134d00b07d8c19954873
jaSunny <jannik@github.com>
2015-01-19 10:47:46 +0100

    added asynchronous call for task form

---
+++ samples/FrontendMockup/v2/form_content.php
@@ -0,0 +1,4 @@
+                        <li>Teil kleben</li>
+                        <li>Teil transportieren</li>
+                        <li>Teil schweißen</li>
+                        <li><font color="green">Teil schrauben</font></li>
