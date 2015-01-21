cdd9994d96b212686b8818808021e24c0df34b2b
jaSunny <jannik@github.com>
2015-01-21 16:33:40 +0100

    improving debugging through better error printing

--- samples/FrontendMockup/v2/core/RESTCalls.php
+++ samples/FrontendMockup/v2/core/RESTCalls.php
@@ -1,32 +1,58 @@
 <?php
-include 'config.php';
+$debug = true;
 
 ###################################################
 #
 #	Functions for JEngine Calls
 function GetActivities($PCM_Scenario, $PCM_Fragment, $PCM_status) {
-	$get_json = file_get_contents($JEngine_Server_URL."/".$JCore_REST_Interface."/".$PCM_Scenario."/".$PCM_Fragment."/".$PCM_status);
+	global $JEngine_Server_URL, $JCore_REST_Interface, $debug;
+	$URL = $JEngine_Server_URL."/".$JCore_REST_Interface."/".$PCM_Scenario."/".$PCM_Fragment."/".$PCM_status;
+	$get_json = file_get_contents($URL);
 	$get_response_as_array = json_decode($get_json,true);
+	if($debug){
+		error_log("HTTP GET on ".$URL);
+		error_log("Returned ".$get_json);
+		error_log("Decoded json as ".$get_response_as_array);
+	}
 	return $get_respone_as_array;
 }
 
 function ShowScenarios() {
-	$get_json = file_get_contents($JEngine_Server_URL."/".$JCore_REST_Interface."/Show");
+	global $JEngine_Server_URL, $JCore_REST_Interface, $debug;
+	$URL = $JEngine_Server_URL."/".$JCore_REST_Interface."/Show";
+	$get_json = file_get_contents($URL);
 	$get_response_as_array = json_decode($get_json,true);
+	if($debug){
+		error_log("HTTP GET on ".$URL);
+		error_log("Returned ".$get_json);
+		error_log("Decoded json as ".$get_response_as_array);
+	}
 	return $get_respone_as_array['ids'];
 }
 
 function ShowScenarioInstances($PCM_Scenario) {
-	$get_json = file_get_contents($JEngine_Server_URL."/".$JCore_REST_Interface."/Instances/".$PCM_Scenario);
+	global $JEngine_Server_URL, $JCore_REST_Interface, $debug;
+	$URL = $JEngine_Server_URL."/".$JCore_REST_Interface."/Instances/".$PCM_Scenario;
+	$get_json = file_get_contents($URL);
 	$get_response_as_array = json_decode($get_json,true);
+	if($debug){
+		error_log("HTTP GET on ".$URL);
+		error_log("Returned ".$get_json);
+		error_log("Decoded json as ".$get_response_as_array);
+	}
 	return $get_respone_as_array['ids'];
 }
 
 function PostActivities($PCM_Scenario, $PCM_Fragment, $PCM_Activity, $PCM_status, $PCM_comment) {
+	global $JEngine_Server_URL, $JCore_REST_Interface, $debug;
 	$URL = $JEngine_Server_URL."/".$JCore_REST_Interface."/".$PCM_Scenario."/".$PCM_Fragment."/".$PCM_Activity."/".$PCM_status."/".$PCM_comment;
 	//$data = array('key1' => 'value1', 'key2' => 'value2');
 	$data = array();
 	$result = PostWrapper($URL, $data);
+	if($debug){
+		error_log("HTTP GET on ".$URL);
+		error_log("Returned ".$result);
+	}
 	if($result){
 		return true;
 	} else {
