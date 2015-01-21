ea98de44eaafec877941eb3b6742024f9047dc49
shaarmann <stephan.haarmann@student.hpi.uni-potsdam.de>
2015-01-21 15:33:33 +0100

    Database Configuration will be loaded from env Variables

--- src/main/java/de/uni_potsdam/hpi/bpt/bp2014/database/Connection.java
+++ src/main/java/de/uni_potsdam/hpi/bpt/bp2014/database/Connection.java
@@ -1,7 +1,8 @@
 package de.uni_potsdam.hpi.bpt.bp2014.database;
 
-import javax.annotation.Resource;
-import javax.sql.DataSource;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import java.io.*;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -12,10 +13,7 @@ public class Connection {
     private static String username;
     private static String password;
     private static String url;
-    public static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-
-    @Resource(name="jdbc/jengine")
-    private DataSource ds;
+    private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
 
     private Connection(){
     }
@@ -31,89 +29,18 @@ public class Connection {
     public static Connection getInstance() {
         if (instance == null) {
             String path = "src/main/resources/database_connection";
-            //instance = new Connection("C:/xampp/tomcat/webapps/JEngine/WEB-INF/classes/database_connection");
             instance = new Connection();
             instance.initializeDatabaseConfiguration(path);
         }
         return instance;
     }
-    private static String getUsername(){
-        FileReader fr = null;
-        try {
-            fr = new FileReader(file);
-        } catch (FileNotFoundException e) {
-            //e.printStackTrace();
-            username = "root";
-            password = "foi6cixoo0Quah2e";
-            url = "jdbc:mysql://127.0.0.1/JEngineV2";
-        }
-        BufferedReader br = new BufferedReader(fr);
-        String username = "";
-        try {
-            username = br.readLine();
-        } catch (IOException e) {
-            //e.printStackTrace();
-            username = "root";
-            password = "foi6cixoo0Quah2e";
-            url = "jdbc:mysql://127.0.0.1/JEngineV2";
-        }
-        return username;
-    }
-    private static String getPassword(){
-        FileReader fr = null;
-        try {
-            fr = new FileReader(file);
-        } catch (FileNotFoundException e) {
-            //e.printStackTrace();
-            username = "root";
-            password = "foi6cixoo0Quah2e";
-            url = "jdbc:mysql://127.0.0.1/JEngineV2";
-        }
-        BufferedReader br = new BufferedReader(fr);
-        String password = "";
-        try {
-            br.readLine();
-            password = br.readLine();
-        } catch (IOException e) {
-            //e.printStackTrace();
-            username = "root";
-            password = "foi6cixoo0Quah2e";
-            url = "jdbc:mysql://127.0.0.1/JEngineV2";
-        }
-        return password;
-    }
-    private static String getUrl(){
-        FileReader fr = null;
-        try {
-            fr = new FileReader(file);
-        } catch (FileNotFoundException e) {
-            username = "root";
-            password = "foi6cixoo0Quah2e";
-            url = "jdbc:mysql://127.0.0.1/JEngineV2";
-        }
-        //System.err.println(file.getAbsoluteFile());
-        BufferedReader br = new BufferedReader(fr);
-        String url = "";
-        try {
-            br.readLine();
-            br.readLine();
-            url = br.readLine();
-        } catch (IOException e) {
-            username = "root";
-            password = "foi6cixoo0Quah2e";
-            url = "jdbc:mysql://127.0.0.1/JEngineV2";
-            //e.printStackTrace();
-        }
-        return url;
-    }
+
 
     public java.sql.Connection connect() {
         java.sql.Connection conn = null;
         try {
-
-            //   return ds.getConnection();
             //Register JDBC driver
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName(JDBC_DRIVER);
             //Open a connection
             conn = DriverManager.getConnection(url, username, password);
         } catch (SQLException se) {
@@ -127,18 +54,24 @@ public class Connection {
     }
 
     private void initializeDatabaseConfiguration(String path) {
-
-        try{
-            file = new File(path);
-            file = file.getAbsoluteFile();
-            username = this.getUsername();
-            password = this.getPassword();
-            url = this.getUrl();
-        }catch(Exception e) {
-            username = "root";
-            password = "foi6cixoo0Quah2e";
-            url = "jdbc:mysql://127.0.0.1/JEngineV2";
+        file = new File(path);
+        try {
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+            username = br.readLine();
+            password = br.readLine();
+            url = br.readLine();
+        } catch (FileNotFoundException e) {
+            try {
+                Context ctx = new InitialContext();
+                username = (String)ctx.lookup("java:comp/env/username");
+                password = (String)ctx.lookup("java:comp/env/password");
+                url = (String)ctx.lookup("java:comp/env/url");
+            } catch (NamingException e1) {
+                e1.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
     }
 }
