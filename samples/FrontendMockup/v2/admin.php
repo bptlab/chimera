7c593ee8961459d32cde22ae48ec3ab9db87d81b
jaSunny <jannik@github.com>
2015-01-21 14:44:26 +0100

    implementing Get & POST functions

--- samples/FrontendMockup/v2/admin.php
+++ samples/FrontendMockup/v2/admin.php
@@ -1,9 +1,21 @@
 <?php
+include 'config.php';
 
+//update Cookie Values in case of POST is set.
+if(isset($_POST["ScenarioID"])){
+  setcookie("JEngine_ScenarioID", $_POST["ScenarioID"]);
+  setcookie("JEngine_ScenarioInstanceID", $_POST["ScenarioInstanceID"]);
+  setcookie("JEngine_ActivityID", $_POST["ActivityID"]);
+  setcookie("JEngine_UserID", $_POST["UserID"]);
+  echo "update successful";
+}
+
+//if cookie is set, do ... else you could automatically generate IDs..
 if(isset($_COOKIE['JEngine_ScenarioInstanceID']) && $_COOKIE['JEngine_ScenarioInstanceID'] === "1") {
 
 }
 
+//print out form for editing values
 echo"<html><body>
      <form action='admin.php' method='post'>
         JEngine_ScenarioID: <input type='text' name='ScenarioID' value='".$_COOKIE[JEngine_ScenarioID]."'><br>
@@ -14,10 +26,18 @@ echo"<html><body>
     </form>
     </body></html> ";
 
-if(isset($_POST["ScenarioID"])){
-  setcookie("JEngine_ScenarioID", $_POST["ScenarioID"]);
-  setcookie("JEngine_ScenarioInstanceID", $_POST["ScenarioInstanceID"]);
-  setcookie("JEngine_ActivityID", $_POST["ActivityID"]);
-  setcookie("JEngine_UserID", $_POST["UserID"]);
-  echo "update successful";
+
+
+
+
+$get_json = file_get_contents($JEngine_Server_URL."/".$JCore_REST_Interface."/Show");
+
+//Exmaple JSON String: '{"ids":[2,4],"label":{"2":"Essen kochen","4":"Zutaten kaufen"}}';
+$get_response = json_decode($get_json,true);
+
+$response_amount_of_ids = count($get_response['ids']);
+$response_label = $get_response['label'];
+
+if($response_amount_of_ids == 0){
+  die (" no ids provided");
 }
