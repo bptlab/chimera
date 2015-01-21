6d4d05f2794c9c6c6aa6ff79118677c4dda6f9e6
jaSunny <jannik@github.com>
2015-01-21 17:16:28 +0100

    fixes at least one issue; now displaying correctly all enabled activites for an instance

--- samples/FrontendMockup/v2/form_content.php
+++ samples/FrontendMockup/v2/form_content.php
@@ -2,16 +2,14 @@
 include 'core/config.php';
 include 'core/RESTCalls.php';
 
+$automatic = true;
+
 $PCM_Scenario = "1";
-$PCM_Fragment = "47";
+$PCM_Fragment = "60";
 $PCM_status = "enabled";
 $PCM_comment = "comment";
 
 $get_response = GetActivities($PCM_Scenario, $PCM_Fragment, $PCM_status);
-//$get_json = file_get_contents($JEngine_Server_URL."/".$JCore_REST_Interface."/".$PCM_Scenario."/".$PCM_Fragment."/".$PCM_status."/".$PCM_comment);
-
-//Exmaple JSON String: '{"ids":[2,4],"label":{"2":"Essen kochen","4":"Zutaten kaufen"}}';
-//$get_response = json_decode($get_json,true);
 
 $response_amount_of_ids = count($get_response['ids']);
 $response_label = $get_response['label'];
@@ -20,10 +18,18 @@ if($response_amount_of_ids == 0){
 	die (" no ids provided");
 }
 
-?>
 
-<li>Teil kleben</li>
-<li>Teil transportieren</li>
-<li>Teil schweißen</li>
-<li><font color="green">Teil schrauben</font></li>
+if($automatic){
+	foreach ($response_label as &$response_label_value) {
+		echo"<li>".$response_label_value."</li>";
+	}
+
+
+} else {
+	echo "
+	<li>Teil kleben</li>
+	<li>Teil transportieren</li>
+	<li>Teil schweißen</li>
+	<li><font color='green'>Teil schrauben</font></li>";
+}
 
