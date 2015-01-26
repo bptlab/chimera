d5e21a42166ed6355d6c7d4863cfcb0bbaa92672
Jaspar Mang <jaspar.mang@student.hpi.uni-potsdam.de>
2015-01-26 10:44:41 +0100

    Updated String to Response in RestConnection [ci skip]

--- src/main/java/de/uni_potsdam/hpi/bpt/bp2014/jcore/RestConnection.java
+++ src/main/java/de/uni_potsdam/hpi/bpt/bp2014/jcore/RestConnection.java
@@ -23,81 +23,80 @@ public class RestConnection {
     public Response showEnabledActivities( @PathParam("Scenarioname") int scenarioID, @PathParam("Instance") int scenarioInstanceID,  @PathParam("Status") String status ){
         if (status.equals("enabled")) {
             if (!executionService.openExistingScenarioInstance(new Integer(scenarioID), new Integer(scenarioInstanceID))){
-                return Response.status(500).build();
-                //return "error: not a correct scenario instance";
+                return Response.serverError().entity("Error: not a correct scenario instance").build();
+                
             }
             LinkedList<Integer> enabledActivitiesIDs = executionService.getEnabledActivitiesIDsForScenarioInstance(scenarioInstanceID);
             HashMap<Integer, String> labels = executionService.getEnabledActivityLabelsForScenarioInstance(scenarioInstanceID);
-            //if(enabledActivitiesIDs.size() == 0) return "empty";
+            if(enabledActivitiesIDs.size() == 0) return Response.serverError().entity("Error: empty").build();
             Gson gson = new Gson();
             JsonHashMapIntegerString json = new JsonHashMapIntegerString(enabledActivitiesIDs, labels);
             String jsonRepresentation = gson.toJson(json);
             return Response.ok(jsonRepresentation, MediaType.APPLICATION_JSON).build();
-        }/*else if(status.equals("terminated")){
-            if(!executionService.existScenarioInstance(scenarioID,scenarioInstanceID)) return "error: not a correct scenario instance";
+        }else if(status.equals("terminated")){
+            if(!executionService.existScenarioInstance(scenarioID,scenarioInstanceID)) return Response.serverError().entity("Error: not a correct scenario instance").build();
             LinkedList<Integer> terminatedActivities = historyService.getTerminatedActivitysForScenarioInstance(scenarioInstanceID);
             HashMap<Integer, String> labels = historyService.getTerminatedActivityLabelsForScenarioInstance(scenarioInstanceID);
-            if(terminatedActivities.size() == 0) return "empty";
+            if(terminatedActivities.size() == 0) return Response.serverError().entity("Error: empty").build();
             Gson gson = new Gson();
             JsonHashMapIntegerString json = new JsonHashMapIntegerString(terminatedActivities, labels);
             String jsonRepresentation = gson.toJson(json);
-            return jsonRepresentation;
+            return Response.ok(jsonRepresentation,MediaType.APPLICATION_JSON).build();
         }
-        return "error: status not clear";*/
-        return Response.ok().build();
+        return Response.serverError().entity("Error: status not clear").build();
     }
 
     @GET
     @Path("DataObjects/{Scenarioname}/{Instance}")
     @Produces(MediaType.APPLICATION_JSON)
-    public String showDataObjects( @PathParam("Scenarioname") int scenarioID, @PathParam("Instance") int scenarioInstanceID,  @PathParam("Status") String status ){
-        if (!executionService.openExistingScenarioInstance(new Integer(scenarioID), new Integer(scenarioInstanceID))) return "error: not a correct scenario instance";
+    public Response showDataObjects( @PathParam("Scenarioname") int scenarioID, @PathParam("Instance") int scenarioInstanceID,  @PathParam("Status") String status ){
+        if (!executionService.openExistingScenarioInstance(new Integer(scenarioID), new Integer(scenarioInstanceID))) return Response.serverError().entity("Error: not a correct scenario instance").build();
         LinkedList<Integer> dataObjects = executionService.getAllDataObjectIDs(scenarioInstanceID);
         HashMap<Integer, String> labels = executionService.getAllDataObjectStates(scenarioInstanceID);
-        if(dataObjects.size() == 0) return "empty";
+        if(dataObjects.size() == 0) return Response.serverError().entity("Error: empty").build();
         Gson gson = new Gson();
         JsonHashMapIntegerString json = new JsonHashMapIntegerString(dataObjects, labels);
         String jsonRepresentation = gson.toJson(json);
-        return jsonRepresentation;
+        return Response.ok(jsonRepresentation,MediaType.APPLICATION_JSON).build();
     }
 
     @GET
     @Path("Show")
     @Produces(MediaType.APPLICATION_JSON)
-    public String showScenarios(){
+    public Response showScenarios(){
         LinkedList<Integer> scenarioIDs = executionService.getAllScenarioIDs();
-        if(scenarioIDs.size() == 0) return "empty";
+        if(scenarioIDs.size() == 0) return Response.serverError().entity("Error: empty").build();
         Gson gson = new Gson();
         JsonIntegerList json = new JsonIntegerList(scenarioIDs);
         String jsonRepresentation = gson.toJson(json);
-        return jsonRepresentation;
+        return Response.ok(jsonRepresentation,MediaType.APPLICATION_JSON).build();
 
     }
 
     @GET
     @Path("Get/ScenarioID/{ScenarioInstance}")
     @Produces(MediaType.APPLICATION_JSON)
-    public String getScenarioID(@PathParam("ScenarioInstance") int scenarioInstanceID){
-        if(!executionService.existScenarioInstance(scenarioInstanceID)) return "error: not a correct scenario instance";
+    public Response getScenarioID(@PathParam("ScenarioInstance") int scenarioInstanceID){
+        if(!executionService.existScenarioInstance(scenarioInstanceID)) return Response.serverError().entity("Error: not a correct scenario instance").build();
         int scenarioID = executionService.getScenarioIDForScenarioInstance(scenarioInstanceID);
         Gson gson = new Gson();
         JsonInteger json = new JsonInteger(scenarioID);
         String jsonRepresentation = gson.toJson(json);
-        return jsonRepresentation;
+        return Response.ok(jsonRepresentation,MediaType.APPLICATION_JSON).build();
 
     }
 
     @GET
     @Path("Instances/{Instance}")
     @Produces(MediaType.APPLICATION_JSON)
-    public String showScenarioInstances(@PathParam("Instance") int scenarioID){
-        if(!executionService.existScenario(scenarioID)) return "error: not a correct scenario";
+    public Response showScenarioInstances(@PathParam("Instance") int scenarioID){
+        if(!executionService.existScenario(scenarioID)) return Response.serverError().entity("Error: not a correct scenario").build();
         LinkedList<Integer> scenarioIDs = executionService.listAllScenarioInstancesForScenario(scenarioID);
-        if(scenarioIDs.size() == 0) return "empty";
+        if(scenarioIDs.size() == 0) return Response.serverError().entity("Error: empty").build();
         Gson gson = new Gson();
         JsonIntegerList json = new JsonIntegerList(scenarioIDs);
         String jsonRepresentation = gson.toJson(json);
-        return jsonRepresentation;
+        return Response.ok(jsonRepresentation,MediaType.APPLICATION_JSON).build();
 
     }
 
