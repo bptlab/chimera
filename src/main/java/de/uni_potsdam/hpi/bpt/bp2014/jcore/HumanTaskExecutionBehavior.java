562f9fc632efab32d095c2d16ba92add818aa5f7
Jaspar Mang <jaspar.mang@student.hpi.uni-potsdam.de>
2015-01-28 14:49:02 +0100

    fix

--- src/main/java/de/uni_potsdam/hpi/bpt/bp2014/jcore/HumanTaskExecutionBehavior.java
+++ src/main/java/de/uni_potsdam/hpi/bpt/bp2014/jcore/HumanTaskExecutionBehavior.java
@@ -1,5 +1,7 @@
 package de.uni_potsdam.hpi.bpt.bp2014.jcore;
 
+import java.util.HashMap;
+
 /***********************************************************************************
 *   
 *   _________ _______  _        _______ _________ _        _______ 
@@ -21,6 +23,8 @@ package de.uni_potsdam.hpi.bpt.bp2014.jcore;
 
 
 public class HumanTaskExecutionBehavior extends TaskExecutionBehavior{
+
+
     public HumanTaskExecutionBehavior(int activityInstance_id, ScenarioInstance scenarioInstance){
         this.activityInstance_id = activityInstance_id;
         this.scenarioInstance = scenarioInstance;
--- src/main/java/de/uni_potsdam/hpi/bpt/bp2014/jcore/TaskExecutionBehavior.java
+++ src/main/java/de/uni_potsdam/hpi/bpt/bp2014/jcore/TaskExecutionBehavior.java
@@ -25,4 +25,6 @@ package de.uni_potsdam.hpi.bpt.bp2014.jcore;
 public class TaskExecutionBehavior {
     protected ScenarioInstance scenarioInstance;
     protected int activityInstance_id;
+
+    public void execute(){}
 }
