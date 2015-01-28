de4789c2f9c997a38b6d25e66935071f2b75d5c1
shaarmann <stephan.haarmann@student.hpi.uni-potsdam.de>
2015-01-28 10:51:46 +0100

    Implemented the IPersistable interface by the jcomparser data classes and made some small refactorings

--- src/main/java/de/uni_potsdam/hpi/bpt/bp2014/jcomparser/xml/IDeserialisable.java
+++ src/main/java/de/uni_potsdam/hpi/bpt/bp2014/jcomparser/xml/IDeserialisable.java
@@ -1,6 +1,5 @@
 package de.uni_potsdam.hpi.bpt.bp2014.jcomparser.xml;
 
-import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 
 /***********************************************************************************
