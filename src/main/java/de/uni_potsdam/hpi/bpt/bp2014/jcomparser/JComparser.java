0ca3c323a3ce5d3199dac523c163e3b36dad083a
shaarmann <stephan.haarmann@student.hpi.uni-potsdam.de>
2015-02-04 17:22:37 +0100

    Fixed wrong return statement The main method of JComparser had a return statement but was of type void

--- src/main/java/de/uni_potsdam/hpi/bpt/bp2014/jcomparser/JComparser.java
+++ src/main/java/de/uni_potsdam/hpi/bpt/bp2014/jcomparser/JComparser.java
@@ -42,7 +42,6 @@ public class JComparser {
 
     public static void main(String[] args) {
         //start JComparser
-        return 1;
     }
 
     public static void writeAllScenariosToDatabase (String processeditor_server_url) throws XPathExpressionException {
