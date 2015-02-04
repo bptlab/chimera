ecba16275d41e69d98416924dc1927fc76c033db
shaarmann <stephan.haarmann@student.hpi.uni-potsdam.de>
2015-02-04 17:21:11 +0100

    Refactored and Added Tests for xml.Edge.java

--- src/test/java/de/uni_potsdam/hpi/bpt/bp2014/jcomparser/xml/EdgeTest.java
+++ src/test/java/de/uni_potsdam/hpi/bpt/bp2014/jcomparser/xml/EdgeTest.java
@@ -13,6 +13,10 @@ public class EdgeTest {
     private Document document = new DocumentImpl();
     private Element dataFlow;
     private Element controlFlow;
+    private Edge dataFlowEdge;
+    private Edge controlFlowEdge;
+    private Node target;
+    private Node source;
 
     // BEGIN: Set-Up
     @Before
@@ -34,63 +38,80 @@ public class EdgeTest {
         controlFlow.appendChild(createProperty("#sourceNode", "88478777"));
         controlFlow.appendChild(createProperty("#targetNode", "1628237226"));
     }
+
+    @Before
+    public void setUpEdges() {
+        dataFlowEdge = new Edge();
+        dataFlowEdge.initializeInstanceFromXML(dataFlow);
+        controlFlowEdge = new Edge();
+        controlFlowEdge.initializeInstanceFromXML(controlFlow);
+        setUpTargetAndSource();
+    }
+
+    public void setUpTargetAndSource() {
+        HashMap<Integer, Node> nodes = new HashMap<Integer, Node>();
+        target = new Node();
+        target.setId(1628237226);
+        nodes.put(1628237226, target);
+        source = new Node();
+        source.setId(88478777);
+        nodes.put(88478777, source);
+        dataFlowEdge.setControlNodes(nodes);
+        controlFlowEdge.setControlNodes(nodes);
+    }
     // END: Set-Up
 
     // BEGIN: Tests
     @Test
     public void testDataFlowDeserialization() {
-        Edge edge = new Edge();
-        edge.initializeInstanceFromXML((org.w3c.dom.Node) dataFlow);
-        Assert.assertEquals("The ID has not been set correctly", 1536668063, edge.getId());
-        Assert.assertEquals("The Lable has not been set correctly", "", edge.getLabel());
+        Assert.assertEquals("The ID has not been set correctly", 1536668063, dataFlowEdge.getId());
+        Assert.assertEquals("The Lable has not been set correctly", "", dataFlowEdge.getLabel());
         Assert.assertEquals("The type has not been set correctly",
                 "net.frapu.code.visualization.bpmn.DataFlow",
-                edge.getType());
-        Assert.assertEquals("The SourceNodeId has not been set correctly", 88478777, edge.getSourceNodeId());
-        Assert.assertEquals("The TargetNodeId has not been set correctly", 1628237226, edge.getTargetNodeId());
+                dataFlowEdge.getType());
+        Assert.assertEquals("The SourceNodeId has not been set correctly", 88478777, dataFlowEdge.getSourceNodeId());
+        Assert.assertEquals("The TargetNodeId has not been set correctly", 1628237226, dataFlowEdge.getTargetNodeId());
     }
 
     @Test
     public void testGetTarget() {
-        Edge edge = new Edge();
-        edge.initializeInstanceFromXML(dataFlow);
-        HashMap<Integer, Node> nodes = new HashMap<Integer, Node>();
-        Node target = new Node();
-        target.setId(1628237226);
-        nodes.put(1628237226, target);
-        edge.setControlNodes(nodes);
         Assert.assertEquals("The target id is not the same as the id of the target node",
                 1628237226,
-                edge.getTarget().getId());
-        Assert.assertTrue("The target is not the node added as a target", edge.getTarget() == target);
+                dataFlowEdge.getTarget().getId());
+        Assert.assertTrue("The target is not the node added as a target", dataFlowEdge.getTarget() == target);
     }
 
     @Test
     public void testGetSource() {
-        Edge edge = new Edge();
-        edge.initializeInstanceFromXML(dataFlow);
-        HashMap<Integer, Node> nodes = new HashMap<Integer, Node>();
-        Node source = new Node();
-        source.setId(88478777);
-        nodes.put(88478777, source);
-        edge.setControlNodes(nodes);
         Assert.assertEquals("The source id is not the same as the id of the source node",
                 88478777,
-                edge.getSource().getId());
-        Assert.assertTrue("The target is not the node added as a source", edge.getSource() == source);
+                dataFlowEdge.getSource().getId());
+        Assert.assertTrue("The target is not the node added as a source", dataFlowEdge.getSource() == source);
     }
 
     @Test
     public void testSequenceFlowDeserialization() {
-        Edge edge = new Edge();
-        edge.initializeInstanceFromXML((org.w3c.dom.Node) controlFlow);
-        Assert.assertEquals("The ID has not been set correctly", 1536668063, edge.getId());
-        Assert.assertEquals("The Lable has not been set correctly", "", edge.getLabel());
+        Assert.assertEquals("The ID has not been set correctly", 1536668063, controlFlowEdge.getId());
+        Assert.assertEquals("The Lable has not been set correctly", "", controlFlowEdge.getLabel());
         Assert.assertEquals("The type has not been set correctly",
                 "net.frapu.code.visualization.bpmn.SequenceFlow",
-                edge.getType());
-        Assert.assertEquals("The SourceNodeId has not been set correctly", 88478777, edge.getSourceNodeId());
-        Assert.assertEquals("The TargetNodeId has not been set correctly", 1628237226, edge.getTargetNodeId());
+                controlFlowEdge.getType());
+        Assert.assertEquals("The SourceNodeId has not been set correctly", 88478777, controlFlowEdge.getSourceNodeId());
+        Assert.assertEquals("The TargetNodeId has not been set correctly", 1628237226, controlFlowEdge.getTargetNodeId());
+    }
+
+    @Test
+    public void testSaveDataFlow() {
+        controlFlowEdge.getSource().setDatabaseID(424242);
+        controlFlowEdge.getTarget().setDatabaseID(212121);
+        dataFlowEdge.save();
+    }
+
+    @Test
+    public void testSaveSequenceFlow() {
+        controlFlowEdge.getSource().setDatabaseID(212121);
+        controlFlowEdge.getTarget().setDatabaseID(424242);
+        controlFlowEdge.save();
     }
     // END: Tests
 
