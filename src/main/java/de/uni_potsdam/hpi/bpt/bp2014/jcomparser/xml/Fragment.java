8d3a6e4759dcb46496ed7e9ae8b581c18fac0a57
shaarmann <stephan.haarmann@student.hpi.uni-potsdam.de>
2015-02-09 14:48:57 +0100

    Refactored Fragment.java. CheckStyle

--- src/main/java/de/uni_potsdam/hpi/bpt/bp2014/jcomparser/xml/Fragment.java
+++ src/main/java/de/uni_potsdam/hpi/bpt/bp2014/jcomparser/xml/Fragment.java
@@ -1,6 +1,7 @@
 package de.uni_potsdam.hpi.bpt.bp2014.jcomparser.xml;
 
 import de.uni_potsdam.hpi.bpt.bp2014.jcomparser.Connector;
+import de.uni_potsdam.hpi.bpt.bp2014.jcore.ControlNodeInstance;
 import org.w3c.dom.NodeList;
 
 import javax.xml.xpath.XPath;
@@ -10,32 +11,55 @@ import javax.xml.xpath.XPathFactory;
 import java.util.*;
 
 /**
- * Represents a Fragment of the XML-Model. It implements the IDeserialisabel interface which allows to initialize
- * a Fragment Object from an XML and the IPersistable Interface which allows to save the Object to the Database.
+ * Represents a Fragment of the XML-Model.
+ * It implements the IDeserialisabel interface,
+ * which allows to initialize a fragment Object
+ * from an XML and the IPersistable Interface,
+ * which allows to save the Object to the Database.
  */
 public class Fragment implements IDeserialisable, IPersistable {
 
-    // The databaseID of the scenario.
+    /**
+     * The databaseID of the scenario.
+     */
     private int scenarioID = -1;
-    // The name of the Fragment
+    /**
+     * The name of the Fragment.
+     */
     private String fragmentName;
-    // The XML of this fragment Model
+    /**
+     * The XML of this fragment Model.
+     */
     private org.w3c.dom.Node fragmentXML;
-    // The Model-XML-Id of the Fragment
+    /**
+     * The Model-XML-Id of the Fragment.
+     */
     private String fragmentID;
-    // A Map which maps Model-XML-Element-IDs to ControlNodes
+    /**
+     * A Map which maps Model-XML-Element-IDs to ControlNodes.
+     */
     private Map<Integer, Node> controlNodes;
-    // The List of Edges created from the FragmentXML
+    /**
+     * The List of Edges created from the FragmentXML.
+     */
     private List<Edge> edges;
-    // The database ID of the fragment
+    /**
+     * The database ID of the fragment.
+     */
     private int databaseID;
-    // A list of all Inputs sets which are used by any Activities inside this Fragment
+    /**
+     * A list of all Inputs sets,
+     * which are used by any Activities inside this Fragment.
+     */
     private List<InputSet> inputSets;
-    // A List of all Outputs sets which are used by any Activities inside this Fragment
+    /**
+     * A List of all Outputs sets,
+     * which are used by any Activities inside this Fragment.
+     */
     private List<OutputSet> outputSets;
 
     @Override
-    public void initializeInstanceFromXML(org.w3c.dom.Node element) {
+    public void initializeInstanceFromXML(final org.w3c.dom.Node element) {
 
         this.fragmentXML = element;
         setFragmentName();
@@ -47,40 +71,50 @@ public class Fragment implements IDeserialisable, IPersistable {
     }
 
     /**
-     * This method extracts the id from the Model-XML and saves it inside the fragmentID field
+     * This method extracts the id from the Model-XML.
+     * It saves the id inside the fragmentID field.
      */
     private void setFragmentID() {
-        XPath xPath =  XPathFactory.newInstance().newXPath();
+        XPath xPath = XPathFactory.newInstance().newXPath();
         String xPathQuery = "/model/@id";
         try {
-            this.fragmentID = xPath.compile(xPathQuery).evaluate(this.fragmentXML);
+            this.fragmentID = xPath
+                    .compile(xPathQuery)
+                    .evaluate(this.fragmentXML);
         } catch (XPathExpressionException e) {
             e.printStackTrace();
         }
     }
 
     /**
-     * generates Sets (I/O) for all Activities which are part of DataFlow inside the Fragment.
+     * generates Sets (I/O) for all Activities,
+     * which are part of DataFlow inside the Fragment.
      * Assert that first all ControlNodes have to be initialized.
      */
     private void generateSets() {
         inputSets = new LinkedList<InputSet>();
         outputSets = new LinkedList<OutputSet>();
         for (Node node : controlNodes.values()) {
-            boolean isInput = false;
-            boolean isOutput = false;
-            for (Edge edge : edges) {
-                if (edge.getSource() == node && edge.getTarget().isDataNode()) isOutput = true;
-                if (edge.getTarget() == node && edge.getSource().isDataNode()) isInput = true;
-                if (isInput && isOutput) break;
-            }
+//            boolean isInput = false;
+//            boolean isOutput = false;
+//            for (Edge edge : edges) {
+//                if (edge.getSource() == node && edge.getTarget().isDataNode()) {
+//                    isOutput = true;
+//                } else if (edge.getTarget() == node && edge.getSource().isDataNode()) {
+//                    isInput = true;
+//                } else {
+//                    break;
+//                }
+//            }
             if (node.isTask()) {
-                InputSet iSet = InputSet.createInputSetForTaskAndEdges(node, edges);
-                if (null != iSet && isInput) {
+                InputSet iSet = InputSet.createInputSetForTaskAndEdges(node,
+                        edges);
+                if (null != iSet) {
                     inputSets.add(iSet);
                 }
-                OutputSet oSet = OutputSet.createOutputSetForTaskAndEdges(node, edges);
-                if (null != oSet && isOutput) {
+                OutputSet oSet = OutputSet.createOutputSetForTaskAndEdges(node,
+                        edges);
+                if (null != oSet) {
                     outputSets.add(oSet);
                 }
             }
@@ -88,16 +122,33 @@ public class Fragment implements IDeserialisable, IPersistable {
     }
 
     /**
-     * Extracts all Edges from the XML and creates Edge objects
+     * Checks if a specific node has an output set or not.
+     *
+     * @param node The node which will be checked
+     * @return true if an output set exists else false.
+     */
+    private boolean nodeHasOutputSet(final Node node) {
+        for (Edge edge : edges) {
+            if (edge.getSource() == node && edge.getTarget().isDataNode()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Extracts all Edges from the XML and creates Edge objects.
      */
     private void generateEdges() {
         try {
             //get all edges from fragmentXML
-            XPath xPath =  XPathFactory.newInstance().newXPath();
+            XPath xPath = XPathFactory.newInstance().newXPath();
             String xPathQuery = "/model/edges/edge";
-            NodeList edgeNodes = (NodeList) xPath.compile(xPathQuery).evaluate(this.fragmentXML, XPathConstants.NODESET);
+            NodeList edgeNodes = (NodeList) xPath
+                    .compile(xPathQuery)
+                    .evaluate(this.fragmentXML, XPathConstants.NODESET);
             this.edges = new ArrayList<Edge>(edgeNodes.getLength());
-            for(int i = 0; i < edgeNodes.getLength(); i++) {
+            for (int i = 0; i < edgeNodes.getLength(); i++) {
                 Edge currentEdge = new Edge();
                 currentEdge.initializeInstanceFromXML(edgeNodes.item(i));
                 currentEdge.setControlNodes(controlNodes);
@@ -109,18 +160,20 @@ public class Fragment implements IDeserialisable, IPersistable {
     }
 
     /**
-     * Extracts all Nodes from the XML and creates Node objects
+     * Extracts all Nodes from the XML and creates Node objects.
      */
     private void generateControlNodes() {
 
         try {
             //get all nodes from fragmentXML
-            XPath xPath =  XPathFactory.newInstance().newXPath();
+            XPath xPath = XPathFactory.newInstance().newXPath();
             String xPathQuery = "/model/nodes/node";
-            NodeList nodes = (NodeList) xPath.compile(xPathQuery).evaluate(this.fragmentXML, XPathConstants.NODESET);
+            NodeList nodes = (NodeList) xPath
+                    .compile(xPathQuery)
+                    .evaluate(this.fragmentXML, XPathConstants.NODESET);
             this.controlNodes = new HashMap<Integer, Node>(nodes.getLength());
 
-            for(int i = 0; i < nodes.getLength(); i++) {
+            for (int i = 0; i < nodes.getLength(); i++) {
                 Node currentNode = new Node();
                 currentNode.initializeInstanceFromXML(nodes.item(i));
                 this.controlNodes.put(currentNode.getId(), currentNode);
@@ -131,27 +184,37 @@ public class Fragment implements IDeserialisable, IPersistable {
     }
 
     /**
-     * This method extracts the name from the Model-XML and saves it inside the fragmentName field
+     * This method extracts the name from the Model-XML.
+     * The name will be saved inside the fragmentName field
      */
     private void setFragmentName() {
 
-        XPath xPath =  XPathFactory.newInstance().newXPath();
+        XPath xPath = XPathFactory.newInstance().newXPath();
         String xPathQuery = "/model/@name";
         try {
-            this.fragmentName = xPath.compile(xPathQuery).evaluate(this.fragmentXML);
+            this.fragmentName = xPath
+                    .compile(xPathQuery)
+                    .evaluate(this.fragmentXML);
         } catch (XPathExpressionException e) {
             e.printStackTrace();
         }
     }
 
-    public void setScenarioID (int id) {
+    /**
+     * Sets the scenario Id.
+     * The scenario Id should be the primary key of the scenario
+     * @param id the primary key of the scenario
+     */
+    public void setScenarioID(final int id) {
         this.scenarioID = id;
     }
 
     @Override
     public int save() {
         Connector conn = new Connector();
-        this.databaseID = conn.insertFragmentIntoDatabase(this.fragmentName, this.scenarioID);
+        this.databaseID = conn.insertFragmentIntoDatabase(
+                this.fragmentName,
+                this.scenarioID);
         for (Node node : controlNodes.values()) {
             node.setFragmentId(databaseID);
             node.save();
@@ -163,6 +226,9 @@ public class Fragment implements IDeserialisable, IPersistable {
         return databaseID;
     }
 
+    /**
+     * Saves the input and output sets to the database.
+     */
     private void saveSet() {
         for (InputSet set : inputSets) {
             set.save();
@@ -173,25 +239,19 @@ public class Fragment implements IDeserialisable, IPersistable {
     }
 
     /**
-     * Returns the list of edges. This is a Composition if you change the list you will change the state of the
-     * Fragment.
+     * Returns the list of edges.
+     * This is a Composition, if you change the list
+     * you will change the state of the Fragment.
      *
      * @return The List of Edges
      */
-    public List<Edge> getEdges () {
+    public List<Edge> getEdges() {
         return this.edges;
     }
 
-    public int getDatabaseID() {
-        return databaseID;
-    }
-
-    public void setDatabaseID(int databaseID) {
-        this.databaseID = databaseID;
-    }
-
     /**
-     * Returns a Map of Node-Ids (from XML) and their nodes. Any changes will manipulate the state of teh Fragment.
+     * Returns a Map of Node-Ids (from XML) and their nodes.
+     * Any changes will manipulate the state of the Fragment.
      *
      * @return Map<XML_ID, ControlNode>
      */
