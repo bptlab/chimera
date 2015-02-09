bb3f240acedf404941d89e1987f4cb21d4cbee58
shaarmann <stephan.haarmann@student.hpi.uni-potsdam.de>
2015-02-09 10:59:29 +0100

    Added package-info for xml package

---
+++ src/main/java/de/uni_potsdam/hpi/bpt/bp2014/jcomparser/xml/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * This package contains classes to parse the model-xml.
+ * Every class represents entities of the model.
+ *
+ * The class Ausprobieren is for debug options only
+ *
+ * @since 0.1
+ */
+package de.uni_potsdam.hpi.bpt.bp2014.jcomparser.xml;
