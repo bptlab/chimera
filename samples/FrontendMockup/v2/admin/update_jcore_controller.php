fcd67887a417bd8b039ed5fa0567c875e281f933
jaSunny <jannik@github.com>
2015-02-16 15:42:45 +0100

    replacing foreach with a loop in order to detect duplicate entries; adding comments

--- samples/FrontendMockup/v2/admin/update_jcore_controller.php
+++ samples/FrontendMockup/v2/admin/update_jcore_controller.php
@@ -2,7 +2,7 @@
 include '../core/config.php';
 include '../core/RESTCalls.php';
 
-//update Cookie Values in case of POST is set.
+//Updating Status to terminate for an pcm_activity
 if(isset($_POST['update_activity_status_terminate'])) {
 
    $result = PostActivities($_POST['pcm_scenarioID'], $_POST['pcm_scenarioinstances'], $_POST['pcm_activity'], "terminate", "");
@@ -13,7 +13,7 @@ if(isset($_POST['update_activity_status_terminate'])) {
       echo "fatal error in POST";
    }
 
-
+//Updating Status to begin for an pcm_activity
 } elseif(isset($_POST['update_activity_status_begin'])) {
 
    $result = PostActivities($_POST['pcm_scenarioID'], $_POST['pcm_scenarioinstances'], $_POST['pcm_activity'], "begin", "");
@@ -23,6 +23,8 @@ if(isset($_POST['update_activity_status_terminate'])) {
    } else {
       echo "fatal error in POST";
    }
+
+//update JEngine_ScenarioID Cookie
 } elseif(isset($_POST['pcm_scenarioID'])){
   
    unset($_COOKIE['JEngine_ScenarioID']);
@@ -31,6 +33,7 @@ if(isset($_POST['update_activity_status_terminate'])) {
    header("Location: admin.php?l=jcore_controller");
    die();
 
+// update JEngine_ScenarioInstanceID
 } elseif(isset($_POST['pcm_scenarioinstances'])){
 
    unset($_COOKIE['JEngine_ScenarioInstanceID']);
@@ -39,6 +42,7 @@ if(isset($_POST['update_activity_status_terminate'])) {
    header("Location: admin.php?l=jcore_controller");
    die();
 
+//create new Instance for a provided ScenarioID
 } elseif(isset($_POST['pcm_scenarioID_new_Instance'])){
    
    $newInstanceID = StartNewInstance($_POST["pcm_scenarioID_new_Instance"]);
@@ -46,6 +50,23 @@ if(isset($_POST['update_activity_status_terminate'])) {
 
    header("Location: admin.php?l=jcore_controller");
    die();
+
+//reseting JEngine_ScenarioID Cookie
+} elseif(isset($_POST['reset_scenarioID'])){
+   
+   unset($_COOKIE['JEngine_ScenarioID']);
+
+   header("Location: admin.php?l=jcore_controller");
+   die();
+
+//reseting JEngine_ScenarioInstanceID Cookie
+} elseif(isset($_POST['reset_scenarioinstanceID'])){
+   
+   unset($_COOKIE['JEngine_ScenarioInstanceID']);
+
+   header("Location: admin.php?l=jcore_controller");
+   die();
+
 } else {
    echo "something went wrong..";
 }
