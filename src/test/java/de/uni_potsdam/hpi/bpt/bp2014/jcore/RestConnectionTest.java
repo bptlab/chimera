8133296385b39a9ed5c9cc6cbaac3e6df884e670
jaSunny <jannik@github.com>
2015-02-18 10:39:27 +0100

    adding json schemas for testing

--- src/test/java/de/uni_potsdam/hpi/bpt/bp2014/jcore/RestConnectionTest.java
+++ src/test/java/de/uni_potsdam/hpi/bpt/bp2014/jcore/RestConnectionTest.java
@@ -1,6 +1,13 @@
 package de.uni_potsdam.hpi.bpt.bp2014.jcore;
 
+import com.jayway.restassured.RestAssured.*;
+import com.jayway.restassured.matcher.RestAssuredMatchers.*;
+
 
 public class RestConnectionTest {
     //test scenario 2
+    //get("/lotto").then().body("lotto.lottoId", equalTo(5));
+    //get("/lotto").then().body("lotto.winners.winnerId", hasItems(23, 54));
+
+    get("/products").then().assertThat().body(matchesJsonSchemaInClasspath("products-schema.json"));
 }
