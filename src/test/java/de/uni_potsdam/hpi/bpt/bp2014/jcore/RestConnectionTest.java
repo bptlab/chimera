0c8280e61d29ccde27aa1ac3ac80f09f294ad5a8
jaSunny <jannik@github.com>
2015-02-19 09:43:04 +0100

    maybe with PowerMockito

--- src/test/java/de/uni_potsdam/hpi/bpt/bp2014/jcore/RestConnectionTest.java
+++ src/test/java/de/uni_potsdam/hpi/bpt/bp2014/jcore/RestConnectionTest.java
@@ -1,5 +1,6 @@
 package de.uni_potsdam.hpi.bpt.bp2014.jcore;
 
+import de.uni_potsdam.hpi.bpt.bp2014.database.Connection;
 import org.easymock.EasyMock;
 import org.easymock.IAnswer;
 import org.junit.Assert;
@@ -7,7 +8,9 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.api.easymock.PowerMock;
+
 import static org.easymock.EasyMock.createNiceMock;
+
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.modules.junit4.PowerMockRunner;
@@ -16,11 +19,14 @@ import static com.jayway.restassured.RestAssured.*;
 import static com.jayway.restassured.matcher.RestAssuredMatchers.*;
 
 import de.uni_potsdam.hpi.bpt.bp2014.config.Config;
+
 import java.util.LinkedList;
 
 import static com.jayway.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.powermock.api.support.membermodification.MemberMatcher.constructor;
+import static org.powermock.api.support.membermodification.MemberModifier.suppress;
 
-@PowerMockIgnore("javax.net.ssl.*")
+@PowerMockIgnore({"javax.net.ssl.*","java.sql.*","com.mysql.jdbc.*"})
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(ExecutionService.class)
 public class RestConnectionTest {
@@ -42,7 +48,32 @@ public class RestConnectionTest {
 
     @Test
     public void testGetScenarios() {
+        suppress(constructor(Connection.class));
+
         String getUrl = "/jcomparser/scenarios/0/";
+
+        // mock all the static methods in a class called "Static"
+        PowerMockito.mockStatic(Static.class);
+        // use Mockito to set up your expectation
+        Mockito.when(Static.firstStaticMethod(param)).thenReturn(value);
+        Mockito.when(Static.secondStaticMethod()).thenReturn(123);
+
+        // execute your test
+        classCallStaticMethodObj.execute();
+
+        // Different from Mockito, always use PowerMockito.verifyStatic() first
+        PowerMockito.verifyStatic(Static.class, Mockito.times(2));
+        // Use EasyMock-like verification semantic per static method invocation
+        Static.firstStaticMethod(param);
+
+        // Remember to call verifyStatic() again
+        PowerMockito.verifyStatic(Static.class); // default is once
+        Static.secondStaticMethod();
+
+        // Again, remember to call verifyStatic()
+        PowerMockito.verifyStatic(Static.class, Mockito.never());
+        Static.thirdStaticMethod();
+
 
         //mock executionService class so we provide what we expect
         try {
@@ -71,7 +102,11 @@ public class RestConnectionTest {
 
         //retrieve GET and check if it is as expected
         get(getUrl).
-                then().assertThat().body(matchesJsonSchemaInClasspath("json/all-scenarios-schema.json"));
+                then().
+                assertThat().
+                body(
+                        matchesJsonSchemaInClasspath("json/all-scenarios-schema.json")
+                );
     }
 
     @Test
