257374aac35263bb58902e36dc80bf08889d0447
jaSunny <jannik@github.com>
2015-02-19 21:33:34 +0100

    working on RestTest.java for JComparser

--- src/test/java/de/uni_potsdam/hpi/bpt/bp2014/jcomparser/RestTest.java
+++ src/test/java/de/uni_potsdam/hpi/bpt/bp2014/jcomparser/RestTest.java
@@ -1,19 +1,51 @@
 package de.uni_potsdam.hpi.bpt.bp2014.jcomparser;
 
 
+import com.ibatis.common.jdbc.ScriptRunner;
+import de.uni_potsdam.hpi.bpt.bp2014.AbstractTest;
 import de.uni_potsdam.hpi.bpt.bp2014.config.Config;
+import de.uni_potsdam.hpi.bpt.bp2014.database.Connection;
 import de.uni_potsdam.hpi.bpt.bp2014.jcomparser.xml.Node;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.w3c.dom.Element;
 
-public class RestTest {
+import javax.ws.rs.core.Application;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class RestTest extends AbstractTest {
     String serverURL = de.uni_potsdam.hpi.bpt.bp2014.config.Config.jcomparserServerUrl;
 
-    public void setUpAndNode() {
+    private static final String OLD_SQL_SEED_FILE = "src/main/resources/JEngineV2.sql";
+    static {
+        SQL_SEED_FILE = "src/main/resources/JEngineV2RESTTest.sql";
+    }
+
+    @AfterClass
+    public static void resetDatabase() throws IOException, SQLException {
+        clearDatabase();
+        ScriptRunner runner = new ScriptRunner(Connection.getInstance().connect(), false, false);
+        runner.runScript(new FileReader(OLD_SQL_SEED_FILE));
+    }
+
+    @Override
+    protected Application configure() {
+        return new ResourceConfig(de.uni_potsdam.hpi.bpt.bp2014.jcore.RestConnection.class);
 
     }
+
+    /* #############################################################################
+     *
+     * HTTP GET REQUEST
+     *
+     * #############################################################################
+     */
+
 
     @Test
     public void testGetScenarios() {
@@ -22,13 +54,20 @@ public class RestTest {
 
     @Test
     public void testGetScenarioImage() {
-        String scenarioID = "000000";
-        String url = serverURL + "jcomparser/launch/" + scenarioID + "/image/";
+       // String scenarioID = "000000";
+       // String url = serverURL + "jcomparser/launch/" + scenarioID + "/image/";
     }
 
+    /* #############################################################################
+      *
+      * HTTP POST REQUEST
+      *
+      * #############################################################################
+      */
+    
     @Test
     public void testPost() {
-       String scenarioID = "000000";
-       String url = serverURL + "jcomparser/launch/" + scenarioID + "/";
+      // String scenarioID = "000000";
+       //String url = serverURL + "jcomparser/launch/" + scenarioID + "/";
     }
 }
