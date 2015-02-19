2a3a5ed80cbf8e892050b35c2ecaf28d69f45a64
jaSunny <jannik@github.com>
2015-02-19 14:15:11 +0100

    splitting RestConnectionTest into part with mock and without one

--- src/test/java/de/uni_potsdam/hpi/bpt/bp2014/jcore/RestConnectionTest.java
+++ src/test/java/de/uni_potsdam/hpi/bpt/bp2014/jcore/RestConnectionTest.java
@@ -1,38 +1,22 @@
 package de.uni_potsdam.hpi.bpt.bp2014.jcore;
 
+import de.uni_potsdam.hpi.bpt.bp2014.config.Config;
 import de.uni_potsdam.hpi.bpt.bp2014.database.Connection;
-//import org.easymock.EasyMock;
-import org.easymock.IAnswer;
-import org.junit.Assert;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.powermock.api.easymock.PowerMock;
 
-//import static org.easymock.EasyMock.createNiceMock;
-
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.core.classloader.annotations.PowerMockIgnore;
-import org.powermock.modules.junit4.PowerMockRunner;
-
-import static com.jayway.restassured.RestAssured.*;
-import static com.jayway.restassured.matcher.RestAssuredMatchers.*;
-
-import de.uni_potsdam.hpi.bpt.bp2014.config.Config;
 
 import java.util.LinkedList;
 
+import static com.jayway.restassured.RestAssured.expect;
+import static com.jayway.restassured.RestAssured.get;
 import static com.jayway.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
-import static org.powermock.api.support.membermodification.MemberMatcher.constructor;
-import static org.powermock.api.support.membermodification.MemberModifier.suppress;
+import static jersey.repackaged.com.google.common.base.Predicates.equalTo;
+import static org.hamcrest.CoreMatchers.hasItems;
 
 
-/**
- *
- */
-@PowerMockIgnore({"javax.net.ssl.*","java.sql.*","com.mysql.jdbc.*"})
-@RunWith(PowerMockRunner.class)
-@PrepareForTest(ExecutionService.class)
 public class RestConnectionTest {
 
     private ExecutionService executionService = new ExecutionService();
@@ -45,9 +29,7 @@ public class RestConnectionTest {
 
     @Before
     public void setUp() {
-        ExecutionService executionService = PowerMock.createPartialMock(ExecutionService.class,
-                GET_ALL_SCENARIOS,
-                LIST_ALL_SCENARIO_INSTANCES);
+
     }
 
     /**
@@ -55,66 +37,13 @@ public class RestConnectionTest {
      */
     @Test
     public void testGetScenarios() {
-        suppress(constructor(Connection.class));
-        //@PowerMock.mockStatic(ExecutionService.class)
-        //@PowerMock.replay(ClassThatContainsStaticMethod.class)
-        //@PowerMock.verify(ClassThatContainsStaticMethod.class)
 
         String getUrl = "/jcomparser/scenarios/0/";
-/*
-        // mock all the static methods in a class called "Static"
-        PowerMockito.mockStatic(Static.class);
-        // use Mockito to set up your expectation
-        Mockito.when(Static.firstStaticMethod(param)).thenReturn(value);
-        Mockito.when(Static.secondStaticMethod()).thenReturn(123);
 
-        // execute your test
-        classCallStaticMethodObj.execute();
-
-        // Different from Mockito, always use PowerMockito.verifyStatic() first
-        PowerMockito.verifyStatic(Static.class, Mockito.times(2));
-        // Use EasyMock-like verification semantic per static method invocation
-        Static.firstStaticMethod(param);
-
-        // Remember to call verifyStatic() again
-        PowerMockito.verifyStatic(Static.class); // default is once
-        Static.secondStaticMethod();
-
-        // Again, remember to call verifyStatic()
-        PowerMockito.verifyStatic(Static.class, Mockito.never());
-        Static.thirdStaticMethod();
-*/
-
-        //mock executionService class so we provide what we expect
-        /*try {
-            PowerMock.mockStatic(ExecutionService.class);
-            PowerMock.replay(ExecutionService.class);
-            PowerMock.expectPrivate(executionService, "getAllScenarioIDs").andAnswer(new IAnswer<LinkedList>() {
-                @Override
-                public LinkedList answer() throws Throwable {
-                    LinkedList ll = new LinkedList();
-                    ll.add(1);
-                    ll.add(2);
-                    ll.add(3);
-                    return (ll);
-                }
-            });
-            PowerMock.verify(ExecutionService.class);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        //retrieve GET and check if it is as expected
-        get(getUrl).
-                then().
-                assertThat().
-                body(
-                        matchesJsonSchemaInClasspath("json/all-scenarios-schema.json")
-
-               );
-               */
-
+        expect().
+                body("ids", hasItems(1, 2, 3)).
+                when().
+                get(getUrl);
     }
 
     /**
