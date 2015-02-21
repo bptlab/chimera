6a9fe7847fd9dfcf2be237003d54354117fbe642
jaSunny <github-notification.plista@mailfish.de>
2015-02-21 15:46:37 +0100

    updating execute_coverity.sh

--- execute_coverity.sh
+++ execute_coverity.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 cd /home/jmang/JEngine
 mvn clean
-cov-build --dir cov-int mvn -DskipTests=true compile
-cov-emit-java --dir cov-int --war /home/jmang/JEngine/target/JEngine.war
+./cov-build --dir cov-int mvn install -Dmaven.test.skip
+./cov-emit-java --dir cov-int --war /home/jmang/JEngine/target/JEngine.war
 tar czvf jengine.tgz cov-int
+curl --form token=qpKLIQAC2s53hJPBaYV9XA \
+  --form email=github@sunny.remote.mx \
+  --form file=jengine.tgz \
+  --form version="v1" \
+  --form description="Work in progress on dev" \
+  https://scan.coverity.com/builds?project=Jengine
