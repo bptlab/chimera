e9a87df29747f90cf00b0309206c2d1e4866bdfc
jaSunny <github@sunny.remote.mx>
2015-03-12 11:09:10 +0100

    JP-93 #comments: finishing up & status quo

--- src/main/webapp/asset/js/modules/scenarioMenu.js
+++ src/main/webapp/asset/js/modules/scenarioMenu.js
@@ -18,6 +18,22 @@
 		};
 	});
 	
+	
+	scenario.controller('FormCtrl', function ($scope, $http) {
+	    
+	    $scope.data = {
+		receiver: "default",
+		subject: "default",
+		message: "default",
+	    };
+
+	    $scope.submitForm = function() {
+		console.log("posting data....");
+		$http.post(JEngine_Server_URL + "/" + JCore_REST_Interface + "/config/emailtask/"+ controller.workingID + "/?", JSON.stringify(data)).success(function(){/*success callback*/});
+	    };
+	});
+
+
 	// Create a Controller for the Scenario Information
 	scenario.controller('ScenarioController', ['$routeParams', '$location', '$http',
 		function($routeParams, $location, $http){
@@ -266,10 +282,6 @@
 						}
 					});     
 		        }
-		        this.submitForm = function() {
-				console.log("posting data....");
-				$http.post(JEngine_Server_URL + "/" + JCore_REST_Interface + "/config/emailtask/"+ controller.workingID + "/?", JSON.stringify(data)).success(function(){/*success callback*/});
-		        };
 			//get all infos for popup
 			this.getDetails = function(id){
 				controller.getDetailsForMailtaskID(id);
