e3f020d83f54b786930e85ad2ace76c26e7bbc5f
shaarmann <stephan.haarmann@student.hpi.uni-potsdam.de>
2015-03-23 13:46:24 +0100

    Added RestInterface for the configuration

--- src/main/java/de/uni_potsdam/hpi/bpt/bp2014/jconfiguration/rest/RestInterface.java
+++ src/main/java/de/uni_potsdam/hpi/bpt/bp2014/jconfiguration/rest/RestInterface.java
@@ -1,11 +1,10 @@
 package de.uni_potsdam.hpi.bpt.bp2014.jconfiguration.rest;
 
 import de.uni_potsdam.hpi.bpt.bp2014.database.DbEmailConfiguration;
+import de.uni_potsdam.hpi.bpt.bp2014.database.DbScenario;
+import de.uni_potsdam.hpi.bpt.bp2014.util.JsonUtil;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -14,11 +13,8 @@ import javax.xml.bind.annotation.XmlRootElement;
  * This class is the rest interface for the JEngine configuration.
  * It holds methods to read and set the email template configurations.
  */
-@Path("v2/config")
+@Path("config/v1")
 public class RestInterface {
-
-
-
 
     /**
      * Updates the email configuration for a specified task.
@@ -42,6 +38,73 @@ public class RestInterface {
         return Response.status(
                 result > 0 ? Response.Status.ACCEPTED : Response.Status.NOT_ACCEPTABLE)
                 .build();
+    }
+
+
+
+    /**
+     * This method provides information about all email Tasks inside
+     * a given scenario.
+     * The information consists of the id and the label.
+     * A Json Object will be returned with an array of ids and a Map
+     * from ids to labels.
+     *
+     * @param scenarioID   The ID of the scenario, its mail tasks will be returned.
+     * @param filterString A Filter String, only mail tasks with a label containing
+     *                     this filter String will be returned.
+     * @return The JSON Object with ids and labels.
+     */
+    @GET
+    @Path("scenario/{scenarioID}/emailtask")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAllEmailTasks(
+            @PathParam("scenarioID") int scenarioID,
+            @QueryParam("filter") String filterString) {
+        DbScenario scenario = new DbScenario();
+        DbEmailConfiguration mail = new DbEmailConfiguration();
+        if (!scenario.existScenario(scenarioID)) {
+            return Response
+                    .status(Response.Status.NOT_FOUND)
+                    .type(MediaType.APPLICATION_JSON)
+                    .entity("{}")
+                    .build();
+        }
+        String jsonRepresentation = JsonUtil.JsonWrapperLinkedList(mail.getAllEmailTasksForScenario(scenarioID));
+        return Response.ok(jsonRepresentation, MediaType.APPLICATION_JSON).build();
+    }
+
+    /**
+     * This method provides information about an email Task.
+     * It will return a JSON-Object with information about the mail
+     * configuration.
+     * A Configuration contains a receiver, a subject and a content.
+     * A Mail task is specified by:
+     *
+     * @param scenarioID The ID of the scenario model.
+     * @param mailTaskID The control node ID of the mail Task.
+     * @return Returns a 404 if the mail Task or scenario does not exist
+     * and a 200 (OK) with a JSON-Object if the emailTask was found.
+     */
+    @GET
+    @Path("scenario/{scenarioID}/emailtask/{emailTaskID}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getEmailTaskConfiguration(
+            @PathParam("scenarioID") int scenarioID,
+            @PathParam("emailTaskID") int mailTaskID) {
+        DbScenario scenario = new DbScenario();
+        DbEmailConfiguration mail = new DbEmailConfiguration();
+        EmailConfigJaxBean mailConfig = new EmailConfigJaxBean();
+        mailConfig.receiver = mail.getReceiverEmailAddress(mailTaskID);
+        if (!scenario.existScenario(scenarioID) || mailConfig.receiver.equals("")) {
+            return Response
+                    .status(Response.Status.NOT_FOUND)
+                    .type(MediaType.APPLICATION_JSON)
+                    .entity("{}")
+                    .build();
+        }
+        mailConfig.content = mail.getMessage(mailTaskID);
+        mailConfig.subject = mail.getSubject(mailTaskID);
+        return Response.ok(mailConfig, MediaType.APPLICATION_JSON).build();
     }
 
     /**
---
+++ src/test/java/de/uni_potsdam/hpi/bpt/bp2014/jconfiguration/rest/RestInterfaceTest.java
@@ -0,0 +1,164 @@
+package de.uni_potsdam.hpi.bpt.bp2014.jconfiguration.rest;
+
+import com.ibatis.common.jdbc.ScriptRunner;
+import de.uni_potsdam.hpi.bpt.bp2014.AbstractTest;
+import de.uni_potsdam.hpi.bpt.bp2014.database.Connection;
+import net.javacrumbs.jsonunit.core.Option;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.SQLException;
+
+import static net.javacrumbs.jsonunit.JsonMatchers.jsonEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+/**
+ * This class is the test for {@link RestInterface}.
+ * It extends the {@link de.uni_potsdam.hpi.bpt.bp2014.AbstractTest} class,
+ * hence the database configuration will be restored afterwards.
+ */
+public class RestInterfaceTest extends AbstractTest {
+
+    private static final String DEVELOPMENT_SQL_SEED_FILE = "src/main/resources/JEngineV2.sql";
+    /**
+     * Sets up the seed file for the test database.
+     */
+    static {
+        TEST_SQL_SEED_FILE = "src/test/resources/JEngineV2RESTTest_new.sql";
+    }
+    /**
+     * The base url of the jcore rest interface.
+     * Allows us to send requests to the {@link de.uni_potsdam.hpi.bpt.bp2014.jcore.rest.RestInterface}.
+     */
+    private WebTarget base;
+
+    @AfterClass
+    public static void resetDatabase() throws IOException, SQLException {
+        clearDatabase();
+        ScriptRunner runner = new ScriptRunner(Connection.getInstance().connect(), false, false);
+        runner.runScript(new FileReader(DEVELOPMENT_SQL_SEED_FILE));
+    }
+
+    @Override
+    protected Application configure() {
+        return new ResourceConfig(de.uni_potsdam.hpi.bpt.bp2014.jconfiguration.rest.RestInterface.class);
+    }
+
+    @Before
+    public void setUpBase() {
+        base = target("config/v1");
+    }
+
+    /**
+     * When you send a GET to {@link RestInterface#getAllEmailTasks(int, String)}
+     * the response should be of type json.
+     */
+    @Test
+    public void testGetMailTasksReturnsJson() {
+        Response response = base.path("scenario/1/emailtask").request().get();
+        assertEquals("The Response code of get all mail tasks was not 200",
+                200, response.getStatus());
+        assertEquals("Get all mail tasks returns a Response with the wrong media Type",
+                MediaType.APPLICATION_JSON, response.getMediaType().toString());
+    }
+
+    /**
+     * When you send a GET to {@link RestInterface#getAllEmailTasks(int, String)} and
+     * the scenario does not contain any mail task an object with no ids will be returned.
+     */
+    @Test
+    public void testGetAllMailTasksIfAbsent() {
+        Response response = base.path("scenario/1/emailtask").request().get();
+        assertThat("Get all mail Tasks returns not an empty JSON Object when the scenario has no mail tasks",
+                "{\"ids\":[]}", jsonEquals(response.readEntity(String.class)));
+    }
+
+    /**
+     * When you send a GET to {@link RestInterface#getAllEmailTasks(int, String)}
+     * the returned JSON Object should be a specified.
+     * {"ids":[1,2,...],"labels":{1:"abcd"...}}}
+     */
+    @Test
+    public void testGetAllMailTasksJSONResponse() {
+        Response response = base.path("scenario/142/emailtask").request().get();
+        assertThat("Get all mail Tasks returns not an empty JSON Object when the scenario has no mail tasks",
+                "{\"ids\":[362]}", jsonEquals(response.readEntity(String.class)));
+    }
+
+    /**
+     * When you send a Get to {@link RestInterface#getAllEmailTasks(int, String)}
+     * and the ScenarioID is invalid a 404 will be returned but the media type is still
+     * JSON.
+     */
+    @Test
+    public void testGetAllMailTasksMissingScenario() {
+        Response response = base.path("scenario/99999/emailtask").request().get();
+        assertEquals("The Response code of get all mail tasks was not 404",
+                404, response.getStatus());
+        assertEquals("Get all mail tasks returns a Response with the wrong media Type",
+                MediaType.APPLICATION_JSON, response.getMediaType().toString());
+    }
+
+    /**
+     * When you send a Get to {@link RestInterface#getEmailTaskConfiguration(int, int)}
+     * with an invalid scenario an empty JSON object should be returned, with a 404.
+     */
+    @Test
+    public void testGetEmailTaskConfigurationMissingScenario() {
+        Response response = base.path("scenario/99999/emailtask/1").request().get();
+        assertEquals("The Response code of get email task configuration was not 404",
+                404, response.getStatus());
+        assertEquals("Get mail task configuration responses with the wrong media Type",
+                MediaType.APPLICATION_JSON, response.getMediaType().toString());
+    }
+
+    /**
+     * When you send a Get to {@link RestInterface#getEmailTaskConfiguration(int, int)}
+     * with an invalid mailTask an empty JSON object should be returned, with a 404.
+     */
+    @Test
+    public void testGetEmailTaskConfigurationMissingMailTask() {
+        Response response = base.path("scenario/1/emailtask/9999").request().get();
+        assertEquals("The Response code of get email task configuration was not 404",
+                404, response.getStatus());
+        assertEquals("Get mail task configuration responses with the wrong media Type",
+                MediaType.APPLICATION_JSON, response.getMediaType().toString());
+    }
+
+    /**
+     * When you send a Get to {@link RestInterface#getEmailTaskConfiguration(int, int)}
+     * a 200 with an json object should be returned
+     */
+    @Test
+    public void testGetEmailTaskReturnsJSON() {
+        Response response = base.path("scenario/142/emailtask/353").request().get();
+        assertEquals("The Response code of get  mail configuration was not 200",
+                200, response.getStatus());
+        assertEquals("Get all mail tasks returns a Response with the wrong media Type",
+                MediaType.APPLICATION_JSON, response.getMediaType().toString());
+    }
+
+    /**
+     * When you send a Get to {@link RestInterface#getEmailTaskConfiguration(int, int)}
+     * a valid json object with "receiver", "content", "subject" should be returned
+     */
+    @Test
+    public void testGetEmailTaskReturnsCorrectJSON() {
+        Response response = base.path("scenario/142/emailtask/353").request().get();
+        assertThat("Get mail Task configuration returns not an valid JSON object",
+                "{\"receiver\":\"bp2014w1@byom.de\",\"subject\":\"Test\",\"content\":\"Test Message\"}",
+                jsonEquals(response.readEntity(String.class)).when(Option.IGNORING_ARRAY_ORDER));
+    }
+
+
+}
