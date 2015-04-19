7ba9f266d890dd363bf1e4dabc3e0566dc64252f
jaSunny <github@sunny.remote.mx>
2015-04-19 22:30:20 +0200

    JP-99 #comments: adding DB helper and JsonUtil

---
+++ src/main/java/de/uni_potsdam/hpi/bpt/bp2014/database/Helper.java
@@ -0,0 +1,331 @@
+package de.uni_potsdam.hpi.bpt.bp2014.database;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.LinkedList;
+
+public class Helper {
+
+    /**
+     * Executes the given select SQL statement and returns the result in List with Integer.
+     *
+     * @param sql         This is a given select SQL Statement.
+     * @param columnLabel This is the label of the column which is used as the result.
+     * @return List with Integer.
+     */
+    public static LinkedList<Integer> executeStatementReturnsListInt(String sql, String columnLabel) {
+        java.sql.Connection conn = Connection.getInstance().connect();
+        Statement stmt = null;
+        ResultSet rs = null;
+        LinkedList<Integer> results = new LinkedList<Integer>();
+        if (conn == null) {
+            return results;
+        }
+
+        try {
+            //Execute a query
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(sql);
+            while (rs.next()) {
+                results.add(rs.getInt(columnLabel));
+            }
+            //Clean-up environment
+            rs.close();
+        } catch (SQLException se) {
+            //Handle errors for JDBC
+            se.printStackTrace();
+        } finally {
+            //finally block used to close resources
+            try {
+                if (stmt != null) {
+                    stmt.close();
+                }
+            } catch (SQLException se2) {
+                se2.printStackTrace();
+            }
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+        return results;
+    }
+
+    /**
+     * Executes the given select SQL statement and returns the result as String.
+     *
+     * @param sql         This is a given select SQL Statement.
+     * @param columnLabel This is the label of the column which is used as the result.
+     * @return String.
+     */
+    public static String executeStatementReturnsString(String sql, String columnLabel) {
+        java.sql.Connection conn = Connection.getInstance().connect();
+        Statement stmt = null;
+        ResultSet rs = null;
+        String results = "";
+        if (conn == null) {
+            return results;
+        }
+        try {
+            //Execute a query
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(sql);
+            if (rs.next()) {
+                results = rs.getString(columnLabel);
+            }
+            //Clean-up environment
+            rs.close();
+        } catch (SQLException se) {
+            //Handle errors for JDBC
+            se.printStackTrace();
+        } finally {
+            //finally block used to close resources
+            try {
+                if (stmt != null) {
+                    stmt.close();
+                }
+            } catch (SQLException se2) {
+                se2.printStackTrace();
+            }
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+        return results;
+    }
+
+    /**
+     * Executes the given select SQL statement and returns the result as int.
+     *
+     * @param sql         This is a given select SQL Statement.
+     * @param columnLabel This is the label of the column which is used as the result.
+     * @return int.
+     */
+    public int executeStatementReturnsInt(String sql, String columnLabel) {
+        java.sql.Connection conn = Connection.getInstance().connect();
+        Statement stmt = null;
+        ResultSet rs = null;
+        int results = -1;
+        if (conn == null) {
+            return results;
+        }
+
+        try {
+            //Execute a query
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(sql);
+            if (rs.next()) {
+                results = rs.getInt(columnLabel);
+            }
+            //Clean-up environment
+            rs.close();
+            stmt.close();
+            conn.close();
+        } catch (SQLException se) {
+            //Handle errors for JDBC
+            se.printStackTrace();
+        } finally {
+            //finally block used to close resources
+            try {
+                if (stmt != null) {
+                    stmt.close();
+                }
+            } catch (SQLException se2) {
+                se2.printStackTrace();
+            }
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+        return results;
+    }
+
+    /**
+     * Executes the given select SQL statement and returns the result as boolean.
+     *
+     * @param sql         This is a given select SQL Statement.
+     * @param columnLabel This is the label of the column which is used as the result.
+     * @return boolean.
+     */
+
+    public boolean executeStatementReturnsBoolean(String sql, String columnLabel) {
+        java.sql.Connection conn = Connection.getInstance().connect();
+        Statement stmt = null;
+        ResultSet rs = null;
+        Boolean results = false;
+        if (conn == null) {
+            return results;
+        }
+        try {
+            //Execute a query
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(sql);
+            if (rs.next()) {
+                results = rs.getBoolean(columnLabel);
+            }
+            //Clean-up environment
+            rs.close();
+        } catch (SQLException se) {
+            //Handle errors for JDBC
+            se.printStackTrace();
+        } finally {
+            //finally block used to close resources
+            try {
+                if (stmt != null) {
+                    stmt.close();
+                }
+            } catch (SQLException se2) {
+                se2.printStackTrace();
+            }
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+        return results;
+    }
+
+    /**
+     * Executes the given select SQL statement.
+     *
+     * @param sql This is a given SQL Statement.
+     * @return true if there is a result for the statement. false if not.
+     */
+    public boolean executeExistStatement(String sql) {
+        java.sql.Connection conn = Connection.getInstance().connect();
+        Statement stmt = null;
+        ResultSet rs = null;
+        if (conn == null) {
+            return false;
+        }
+
+        try {
+            //Execute a query
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(sql);
+            if (rs.next()) {
+                return true;
+            }
+            //Clean-up environment
+            rs.close();
+        } catch (SQLException se) {
+            //Handle errors for JDBC
+            se.printStackTrace();
+        } finally {
+            //finally block used to close resources
+            try {
+                if (stmt != null) {
+                    stmt.close();
+                }
+            } catch (SQLException se2) {
+                se2.printStackTrace();
+            }
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Executes the given insert SQL statement.
+     *
+     * @param sql This is a given SQL Statement.
+     * @return the generated key for the insert statement.
+     */
+    public int executeInsertStatement(String sql) {
+        java.sql.Connection conn = Connection.getInstance().connect();
+        Statement stmt = null;
+        ResultSet rs = null;
+        if (conn == null) {
+            return -1;
+        }
+        int result = -1;
+        try {
+            //Execute a query
+            stmt = conn.createStatement();
+            stmt.executeUpdate(sql, Statement.RETURN_GENERATED_KEYS);
+            rs = stmt.getGeneratedKeys();
+            if (rs.next()) {
+                result = rs.getInt(1);
+            }
+        } catch (SQLException se) {
+            //Handle errors for JDBC
+            se.printStackTrace();
+        } finally {
+            //finally block used to close resources
+            try {
+                if (stmt != null) {
+                    stmt.close();
+                }
+            } catch (SQLException se2) {
+                se2.printStackTrace();
+            }
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Executes the given SQL Statement. Which should be a update or insert statement.
+     *
+     * @param sql This is a given SQL Statement.
+     */
+    public void executeUpdateStatement(String sql) {
+        java.sql.Connection conn = Connection.getInstance().connect();
+        Statement stmt = null;
+        ResultSet rs = null;
+        if (conn == null) {
+            return;
+        }
+        try {
+            //Execute a querystmt = conn.createStatement();
+            stmt = conn.createStatement();
+            stmt.executeUpdate(sql);
+        } catch (SQLException se) {
+            //Handle errors for JDBC
+            se.printStackTrace();
+        } finally {
+            //finally block used to close resources
+            try {
+                if (stmt != null) {
+                    stmt.close();
+                }
+            } catch (SQLException se2) {
+                se2.printStackTrace();
+            }
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/main/java/de/uni_potsdam/hpi/bpt/bp2014/rest/JsonUtil.java
@@ -0,0 +1,134 @@
+package de.uni_potsdam.hpi.bpt.bp2014.rest;
+
+import com.google.gson.Gson;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+
+/**
+ * This class provides the helper methods/classes used for the JSON representation for the REST connections.
+ */
+public class JsonUtil {
+    /**
+     * @param content contains a LinkedList.
+     * @return a wrapped json.
+     */
+    public static String JsonWrapperLinkedList(LinkedList<Integer> content) {
+        Gson gson = new Gson();
+        JsonIntegerList json = new JsonIntegerList(content);
+        return gson.toJson(json);
+    }
+
+    /**
+     * @param content contains a LinkedList.
+     * @param labels  contains a String.
+     * @return a wrapped json
+     */
+    public static String JsonWrapperHashMap(LinkedList<Integer> content, HashMap<Integer, String> labels) {
+        Gson gson = new Gson();
+        JsonHashMapIntegerString json = new JsonHashMapIntegerString(content, labels);
+        return gson.toJson(json);
+    }
+
+    /**
+     * @param content contains a LinkedList
+     * @param labels  contains a String
+     * @param states  contains a String
+     * @return a wrapped json
+     */
+    public static String JsonWrapperMultipleHashMap(LinkedList<Integer> content, HashMap<Integer, String> labels, HashMap<Integer, String> states) {
+        Gson gson = new Gson();
+        JsonHashMapMultipleIntegerString json = new JsonHashMapMultipleIntegerString(content, labels, states);
+        return gson.toJson(json);
+    }
+    /**
+     * This class is a JSON HashMap containing a List of ID's and a HashMap consisting of the given ID's and labels as Strings.
+     */
+    public static class JsonHashMapIntegerString {
+        private LinkedList<Integer> ids;
+        private HashMap<Integer, String> label;
+
+        /**
+         * constructor.
+         *
+         * @param ids This are the database ID's for the given Objects in the HashMap.
+         * @param labels This is a HashMap of database ID's and the corresponding labels as a String.
+         */
+        public JsonHashMapIntegerString(LinkedList<Integer> ids, HashMap<Integer, String> labels) {
+            this.ids = ids;
+            this.label = labels;
+        }
+    }
+
+    public static class JsonHashMapMultipleIntegerString {
+        private HashMap<Integer, String> states;
+        private LinkedList<Integer> ids;
+        private HashMap<Integer, String> label;
+
+        public JsonHashMapMultipleIntegerString(LinkedList<Integer> ids, HashMap<Integer, String> labels, HashMap<Integer, String> states) {
+            this.ids = ids;
+            this.label = labels;
+            this.states = states;
+        }
+    }
+    /**
+     * This class handles the JSON representation of a list filled with database ID's.
+     */
+    public static class JsonIntegerList {
+        private LinkedList<Integer> ids;
+
+        /**
+         * constructor.
+         *
+         * @param ids These are the database ID's of given objects.
+         */
+        public JsonIntegerList(LinkedList<Integer> ids) {
+            this.ids = ids;
+        }
+    }
+
+    /**
+     * This class is the representation of an Integer as JSON.
+     */
+    public static class JsonInteger {
+        private Integer id;
+
+        /**
+         * constructor.
+         *
+         * @param id Integer Value of a database ID.
+         */
+        public JsonInteger(Integer id) {
+            this.id = id;
+        }
+    }
+
+    /**
+     * This class is the representation of a HashMap that maps a String to a String.
+     */
+    public static class JsonStringHashMap {
+        private HashMap<String, String> ids;
+
+        /**
+         * constructor.
+         *
+         * @param ids This is a HashMap of names as a String and database ID's as a String.
+         */
+        public JsonStringHashMap(HashMap<String, String> ids) {
+            this.ids = ids;
+        }
+    }
+
+    /**
+     *
+     * @param jsonLine
+     * @return parsed json string
+     */
+    public static Map parse(String jsonLine) {
+        Map jsonJavaRootObject = new Gson().fromJson(jsonLine, Map.class);
+        return jsonJavaRootObject;
+        
+    }
+}
