0e79c14c4fc4db5477cd7636e26c6bfabc6e950f
jaSunny <github@sunny.remote.mx>
2015-04-20 10:07:01 +0200

    JP-99 #comments: adding tests for get & delete

--- src/test/java/de/uni_potsdam/hpi/bpt/bp2014/rest/RestInterfaceTest.java
+++ src/test/java/de/uni_potsdam/hpi/bpt/bp2014/rest/RestInterfaceTest.java
@@ -73,7 +73,46 @@ public class RestInterfaceTest extends AbstractTest {
         Response response = base.path("user").request().get();
         assertEquals("The Response code of get Scenario was not 200",
                 200, response.getStatus());
-        assertEquals("Get Scenarios returns a Response with the wrong media Type",
+        assertEquals("Get user returns a Response with the wrong media Type",
                 MediaType.APPLICATION_JSON, response.getMediaType().toString());
+    }
+
+    @Test
+    public void testGetAllUserContentCheck() {
+        Response response = base.path("user").request().get();
+        assertThat("Get Scenarios did not contain the expected information",
+                "{\"myArrayList\":[{\"map\":{\"id\":1,\"username\":\"max\",\"role_id\":1,\"description\":\"Max der Gro?e\"}},{\"map\":{\"id\":2,\"username\":\"robert\",\"role_id\":1,\"description\":\"Mitarbeiter des Monats, admin der rolle 1\"}},{\"map\":{\"id\":3,\"username\":\"Lisa\",\"role_id\":1,\"description\":\"\"}},{\"map\":{\"id\":4,\"username\":\"Steffi\",\"role_id\":10,\"description\":\"Manager\"}},{\"map\":{\"id\":5,\"username\":\"Rolf\",\"role_id\":10,\"description\":\"Top Manager\"}}]}\n",
+                jsonEquals(response.readEntity(String.class)).when(Option.IGNORING_ARRAY_ORDER));
+    }
+
+    @Test
+    public void testGetAllRoleStatusCheck() {
+        Response response = base.path("role").request().get();
+        assertEquals("The Response code of get Scenario was not 200",
+                200, response.getStatus());
+        assertEquals("Get user returns a Response with the wrong media Type",
+                MediaType.APPLICATION_JSON, response.getMediaType().toString());
+    }
+
+    @Test
+    public void testGetAllRoleContentCheck() {
+        Response response = base.path("role").request().get();
+        assertThat("Get Scenarios did not contain the expected information",
+                "{\"myArrayList\":[{\"map\":{\"id\":1,\"rolename\":\"service_mitarbeiter\",\"description\":\"Die Bearbeiten Antr?ge und rufen Leute an\",\"admin_id\":2}},{\"map\":{\"id\":10,\"rolename\":\"manager\",\"description\":\"Die koordinieren und managen die Aufgaben\",\"admin_id\":0}}]}\n",
+                jsonEquals(response.readEntity(String.class)).when(Option.IGNORING_ARRAY_ORDER));
+    }
+
+    @Test
+    public void testDeleteUserStatusCheck() {
+        Response response = base.path("user/1/").request().delete();
+        assertEquals("The Response code of deleting a user was not 200",
+                202, response.getStatus());
+    }
+
+    @Test
+    public void testDeleteRoleStatusCheck() {
+        Response response = base.path("role/1/").request().delete();
+        assertEquals("The Response code of deleting a user was not 200",
+                202, response.getStatus());
     }
 }
