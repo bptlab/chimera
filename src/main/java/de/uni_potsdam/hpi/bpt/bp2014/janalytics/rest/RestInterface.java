8ad1ce5db0c9a171b68c1f6595eb01272b49cf74
jaSunny <github@sunny.remote.mx>
2015-04-28 22:15:54 +0200

    minor changes in FMC diagram and analytics framework

--- src/main/java/de/uni_potsdam/hpi/bpt/bp2014/janalytics/rest/RestInterface.java
+++ src/main/java/de/uni_potsdam/hpi/bpt/bp2014/janalytics/rest/RestInterface.java
@@ -5,6 +5,7 @@ import org.json.JSONObject;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -18,10 +19,20 @@ public class RestInterface {
     private AnalyticsService analyticsService = new AnalyticsService();
 
     @GET
-    @Path("test")
+    @Path("algorithm/{algorithmID}/instance/{instanceID}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getAnalyticsResult() {
-        //TODO: do something
+    public Response getAnalyticsResult(@PathParam("algorithmID") int algorithmID,
+                                       @PathParam("instanceID") int instanceID) {
+        switch (algorithmID) {
+            case 1:
+                Boolean result = analyticsService.getAnalysisResultForInstance(instanceID);
+                break;
+            default:
+                return Response.status(Response.Status.NOT_FOUND)
+                        .type(MediaType.APPLICATION_JSON)
+                        .entity("{\"error\":\"The algorithm is not supported: " + algorithmID + "\"}")
+                        .build();
+        }
         return Response
                 .ok()
                 .type(MediaType.APPLICATION_JSON)
