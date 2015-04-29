d1c9acfe4fa3440595c5974e817bb6956d390b0a
jaSunny <github@sunny.remote.mx>
2015-04-29 10:37:16 +0200

    finally..

--- src/main/java/de/uni_potsdam/hpi/bpt/bp2014/database/DbHistoryDataObjectInstance.java
+++ src/main/java/de/uni_potsdam/hpi/bpt/bp2014/database/DbHistoryDataObjectInstance.java
@@ -45,8 +45,9 @@ public class DbHistoryDataObjectInstance extends DbObject {
      * @return a Map with a Map of the log entries' attribute names as keys and their respective values.
      */
     public Map<Integer, Map<String, Object>> getLogEntriesForScenarioInstance(int scenarioInstanceId){
-        String sql = "SELECT h.id, h.scenarioinstance_id, h.timestamp, h.dataobjectinstance_id, do.name, ns.name as newstate_name, os.name as oldstate_name FROM historydataobjectinstance AS h, dataobjectinstance AS doi, dataobject AS do, state AS ns, state AS os WHERE h.scenarioinstance_id = '"+scenarioInstanceId+"' AND ns.id = 'h.newstate_id' AND os.id = 'h.oldstate_id' ORDER BY timestamp DESC";
-        return this.executeStatementReturnsMapWithMapWithKeys(sql, "h.id", "h.scenarioinstance_id", "do.name", "h.timestamp", "h.dataobjectinstance_id", "oldstate_name", "newstate_name");
+        //String sql ="SELECT h.id, h.scenarioinstance_id, h.timestamp, h.oldstate_id, h.newstate_id, h.dataobjectinstance_id, do.name, ns.name as newstate_name, os.name as oldstate_name FROM historydataobjectinstance AS h, dataobjectinstance AS doi, dataobject AS do, state as ns, state as os WHERE h.scenarioinstance_id = " + scenarioInstanceId + " and ns.id = 'h.newstate_id' and os.id = 'h.oldstate_id' ORDER BY timestamp DESC";
+        String sql = "SELECT h.id, h.scenarioinstance_id, h.timestamp, h.oldstate_id, h.newstate_id, h.dataobjectinstance_id, do.name, ns.name AS newstate_name, os.name AS oldstate_name FROM historydataobjectinstance AS h, dataobjectinstance AS doi, dataobject AS do, state AS ns, state AS os WHERE h.scenarioinstance_id = " + scenarioInstanceId + " AND ns.id = h.newstate_id AND os.id = h.oldstate_id ORDER BY timestamp DESC";
+        return this.executeStatementReturnsMapWithMapWithKeys(sql, "h.id", "h.oldstate_id", "h.newstate_id", "h.scenarioinstance_id", "do.name", "h.timestamp", "h.dataobjectinstance_id", "oldstate_name", "newstate_name");
     }
 
 
