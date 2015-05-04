19682a1ced360f5c4ff5efc3bec96b59c6cbca32
Jaspar Mang <jaspar.mang@student.hpi.uni-potsdam.de>
2015-05-04 13:26:14 +0200

    added webservice POST test

--- src/test/java/de/uni_potsdam/hpi/bpt/bp2014/jcore/WebServiceAcceptanceTest.java
+++ src/test/java/de/uni_potsdam/hpi/bpt/bp2014/jcore/WebServiceAcceptanceTest.java
@@ -1,6 +1,7 @@
 package de.uni_potsdam.hpi.bpt.bp2014.jcore;
 
 import de.uni_potsdam.hpi.bpt.bp2014.database.DbActivityInstance;
+import de.uni_potsdam.hpi.bpt.bp2014.database.DbScenarioInstance;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.test.JerseyTest;
 import org.junit.Test;
@@ -57,5 +58,38 @@ public class WebServiceAcceptanceTest extends JerseyTest{
         }
         assertEquals("[9450]", value12);
         assertEquals("Schmutzgrad pruefen", value13);
+    }
+
+    @Test
+    public void testPOST(){
+        System.out.println("\n ------------------ test Scenario 157 ------------------\n");
+        DbActivityInstance dbActivityInstance = new DbActivityInstance();
+        ExecutionService executionService = new ExecutionService();
+        int scenarioInstance = executionService.startNewScenarioInstance(157);
+        int activity1 = 528;
+
+        System.out.println("Start Scenario 157");
+        System.out.println("enabled Activities: " + executionService.getEnabledActivitiesIDsForScenarioInstance(scenarioInstance).toString());
+        assertArrayEquals(new Integer[]{activity1}, executionService.getEnabledActivitiesIDsForScenarioInstance(scenarioInstance).toArray());
+
+        //do activity 1
+        System.out.println("do activity " + activity1);
+        executionService.beginActivity(scenarioInstance, activity1);
+        assertArrayEquals(new Integer[]{}, executionService.getEnabledActivitiesIDsForScenarioInstance(scenarioInstance).toArray());
+        int activity1instance_id = executionService.getScenarioInstance(scenarioInstance).getRunningControlNodeInstances().getFirst().getControlNodeInstance_id();
+        executionService.setDataAttributeValues(scenarioInstance, activity1instance_id, new HashMap<Integer, String>());
+        executionService.terminateActivity(scenarioInstance, activity1);
+        assertArrayEquals(new Integer[]{activity1}, executionService.getEnabledActivitiesIDsForScenarioInstance(scenarioInstance).toArray());
+        System.out.println("enabled Activities: " + executionService.getEnabledActivitiesIDsForScenarioInstance(scenarioInstance).toString());
+
+        Collection<DataAttributeInstance> dataAttributeInstances = executionService.getScenarioInstance(scenarioInstance).getDataAttributeInstances().values();
+        String value14 = "";
+        for (DataAttributeInstance dataAttributeInstance : dataAttributeInstances){
+            if (dataAttributeInstance.getDataAttribute_id() == 14){
+                value14 = dataAttributeInstance.getValue().toString();
+            }
+        }
+        DbScenarioInstance dbScenarioInstance = new DbScenarioInstance();
+        assertEquals(dbScenarioInstance.getScenarioInstances(156).getLast().toString(), value14);
     }
 }
