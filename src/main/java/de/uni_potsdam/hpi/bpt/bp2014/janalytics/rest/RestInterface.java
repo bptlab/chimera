f78a5e809a0b111154eed1f7fe203888906a6b11
jaSunny <github@sunny.remote.mx>
2015-05-06 09:45:35 +0200

    fixes comiling error

--- src/main/java/de/uni_potsdam/hpi/bpt/bp2014/janalytics/rest/RestInterface.java
+++ src/main/java/de/uni_potsdam/hpi/bpt/bp2014/janalytics/rest/RestInterface.java
@@ -3,10 +3,7 @@ package de.uni_potsdam.hpi.bpt.bp2014.janalytics.rest;
 import de.uni_potsdam.hpi.bpt.bp2014.janalytics.AnalyticsService;
 import org.json.JSONObject;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
