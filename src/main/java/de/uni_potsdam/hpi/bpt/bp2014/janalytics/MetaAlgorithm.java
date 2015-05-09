6c874b8a363d943b02b7bafa29c5a90a56e4bac1
jaSunny <jaSunny@users.noreply.github.com>
2015-05-09 16:35:12 +0200

    Update MetaAlgorithm.java

--- src/main/java/de/uni_potsdam/hpi/bpt/bp2014/janalytics/MetaAlgorithm.java
+++ src/main/java/de/uni_potsdam/hpi/bpt/bp2014/janalytics/MetaAlgorithm.java
@@ -1,5 +1,7 @@
 package de.uni_potsdam.hpi.bpt.bp2014.janalytics;
 
+import org.apache.log4j.Logger;
+
 public class MetaAlgorithm {
   static Logger log = Logger.getLogger(DbObject.class.getName());
 }
