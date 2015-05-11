425214c2902820b0edfe0ff4e2b0bfc9ac80aeb4
jnslk <jan.selke@student.hpi.uni-potsdam.de>
2015-05-11 15:56:21 +0200

    conflict

--- src/main/java/de/uni_potsdam/hpi/bpt/bp2014/janalytics/AnalyticsModel.java
+++ src/main/java/de/uni_potsdam/hpi/bpt/bp2014/janalytics/AnalyticsModel.java
@@ -1,6 +1,7 @@
 package de.uni_potsdam.hpi.bpt.bp2014.janalytics;
 
 import de.uni_potsdam.hpi.bpt.bp2014.database.Connection;
+import de.uni_potsdam.hpi.bpt.bp2014.jcomparser.xml.Scenario;
 import org.apache.log4j.Logger;
 
 import java.sql.ResultSet;
@@ -27,6 +28,24 @@ public class AnalyticsModel extends MetaAnalyticsModel {
         result.add(ActivityLog);
         result.add(DataAttributeLog);
         result.add(DataObjectLog);
+
+        return result;
+    }
+
+    public static ArrayList<Map<Integer, Map<String, Object>>> calculateMeanScenarioInstanceRunTime(int scenario_id){
+        ArrayList<Map<Integer, Map<String, Object>>> result = new ArrayList<Map<Integer, Map<String, Object>>>();
+
+        Map<Integer, Map<String, Object>> scenarioInstancesForScenario = getScenarioInstancesForScenario(scenario_id);
+        result.add (scenarioInstancesForScenario);
+
+        return result;
+    }
+
+    public static ArrayList<Map<Integer, Map<String, Object>>> calculateScenarioInstanceRunTime(int scenarioInstance_id){
+        ArrayList<Map<Integer, Map<String, Object>>> result = new ArrayList<Map<Integer, Map<String, Object>>>();
+
+        Map<Integer, Map<String, Object>> timestampsForScenarioInstance = getLogTimestampsForScenarioInstance(scenarioInstance_id);
+        result.add (timestampsForScenarioInstance);
 
         return result;
     }
