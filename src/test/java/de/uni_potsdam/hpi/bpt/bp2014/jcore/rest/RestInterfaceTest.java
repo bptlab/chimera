49665e5be54b0cc65022393c449c6928a1b85e72
shaarmann <stephan.haarmann@student.hpi.uni-potsdam.de>
2015-05-11 15:35:51 +0200

    Ignoring additional filed for dataobject

--- src/test/java/de/uni_potsdam/hpi/bpt/bp2014/jcore/rest/RestInterfaceTest.java
+++ src/test/java/de/uni_potsdam/hpi/bpt/bp2014/jcore/rest/RestInterfaceTest.java
@@ -632,7 +632,8 @@ public class RestInterfaceTest extends AbstractTest {
         assertThat("The returned JSON does not contain the expected content",
                 "{\"label\":\"object1\",\"id\":1,\"state\":\"init\"}",
                 jsonEquals(response.readEntity(String.class))
-                        .when(Option.IGNORING_ARRAY_ORDER).when(Option.IGNORING_EXTRA_FIELDS));
+                        .when(Option.IGNORING_ARRAY_ORDER)
+                        .when(Option.IGNORING_EXTRA_FIELDS));
     }
 
     /**
@@ -686,7 +687,9 @@ public class RestInterfaceTest extends AbstractTest {
         assertThat("The returned JSON does not contain the expected content",
                 "{\"label\":\"object1\",\"id\":1,\"state\":\"init\"}",
                 jsonEquals(response.readEntity(String.class))
-                        .when(Option.IGNORING_ARRAY_ORDER).when(Option.IGNORING_EXTRA_FIELDS));
+                        .when(Option.IGNORING_ARRAY_ORDER)
+                        .when(Option.IGNORING_ARRAY_ORDER));
+        ;
     }
 
 
@@ -1084,8 +1087,8 @@ public class RestInterfaceTest extends AbstractTest {
                 MediaType.APPLICATION_JSON, response.getMediaType().toString());
         assertThat("The returned JSON does not contain the expected content",
                 response.readEntity(String.class),
-                jsonEquals("[{\"label\":\"Reiseplan\",\"id\":675,\"state\":\"Ziel festgelegt\",\"attributeConfiguration\":[{\"id\":1,\"name\":\"Preis\",\"type\":\"\",\"value\":\"250\"}]}]")
-                        .when(Option.IGNORING_ARRAY_ORDER).when(Option.IGNORING_EXTRA_FIELDS));
+                jsonEquals("[{\"label\":\"Reiseplan\",\"id\":675,\"state\":\"init\",\"attributeConfiguration\":[{\"id\":1,\"name\":\"Preis\",\"type\":\"\",\"value\":\"250\"}]}]")
+                        .when(Option.IGNORING_ARRAY_ORDER));
     }
     /**
      * when you send a get to {@link RestInterface#getOutputDataObjectsAndAttributes(int, int, int)}
@@ -1139,7 +1142,7 @@ public class RestInterfaceTest extends AbstractTest {
         assertThat("The returned JSON does not contain the expected content",
                 response.readEntity(String.class),
                 jsonEquals("[{\"label\":\"Reiseplan\",\"id\":675,\"state\":\"init\",\"attributeConfiguration\":[{\"id\":1,\"name\":\"Preis\",\"type\":\"\",\"value\":\"250\"}]}]")
-                        .when(Option.IGNORING_ARRAY_ORDER).when(Option.IGNORING_EXTRA_FIELDS));
+                        .when(Option.IGNORING_ARRAY_ORDER));
     }
     /**
      * when you send a get to {@link RestInterface#getInputDataObjectsAndAttributes(int, int, int)}
