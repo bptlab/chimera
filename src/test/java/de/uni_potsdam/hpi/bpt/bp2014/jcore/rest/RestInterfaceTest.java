a612f32709b377084c03c870037ac0b63db77ecd
svenihde <sven.ihde@student.hpi.uni-potsdam.de>
2015-05-11 14:03:58 +0200

    fixed test

--- src/test/java/de/uni_potsdam/hpi/bpt/bp2014/jcore/rest/RestInterfaceTest.java
+++ src/test/java/de/uni_potsdam/hpi/bpt/bp2014/jcore/rest/RestInterfaceTest.java
@@ -1084,7 +1084,7 @@ public class RestInterfaceTest extends AbstractTest {
                 MediaType.APPLICATION_JSON, response.getMediaType().toString());
         assertThat("The returned JSON does not contain the expected content",
                 response.readEntity(String.class),
-                jsonEquals("[{\"label\":\"Reiseplan\",\"id\":675,\"state\":\"init\",\"attributeConfiguration\":[{\"id\":1,\"name\":\"Preis\",\"type\":\"\",\"value\":\"250\"}]}]")
+                jsonEquals("[{\"label\":\"Reiseplan\",\"id\":675,\"state\":\"Ziel festgelegt\",\"attributeConfiguration\":[{\"id\":1,\"name\":\"Preis\",\"type\":\"\",\"value\":\"250\"}]}]")
                         .when(Option.IGNORING_ARRAY_ORDER));
     }
     /**
