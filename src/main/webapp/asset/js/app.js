0a7e8dc5daa68750333f6acdc1dbab7ef661e23d
jaSunny <github@sunny.remote.mx>
2015-05-12 11:52:52 +0200

    some propper layouting

--- src/main/webapp/asset/js/app.js
+++ src/main/webapp/asset/js/app.js
@@ -7,49 +7,49 @@ var JHistory_REST_Interface = "JEngine/api/history/v2";
 var JComparser_REST_Interface = "JEngine/api/jcomparser";
 var JUserManagement_REST_Interface = "JUserManagement/api/interface/v1";
 
-(function(){
-	// vars defining the URIs of the REST-APIs
+(function () {
+    // vars defining the URIs of the REST-APIs
     var jfrontend = angular.module('jfrontend', [
-		'ngRoute',
-		'adminConfiguration',
-		'userInteraction']);
-		
-	// Defining Routes for the AngularJS App
-	jfrontend.config(['$routeProvider',
-		function($routeProvider){
-			$routeProvider.
-			// Routes for jCore
-				when('/scenario', {
-					templateUrl: 'asset/templates/scenario.html',
-					controller: 'ScenarioController',
-					controllerAs: 'scenarioCtrl'
-				}).
-				when('/scenario/:id', {
-					templateUrl: 'asset/templates/scenarioDetails.html',
-					controller: 'ScenarioController',
-					controllerAs: 'scenarioCtrl'
-				}).
-				when('/scenario/:id/instance', {
-					templateUrl: 'asset/templates/scenarioInstance.html',
-					controller: 'ScenarioInstanceController',
-					controllerAs: 'instanceCtrl'
-				}).
-				when('/scenario/:id/instance/:instanceId', {
-					templateUrl: 'asset/templates/scenarioInstanceDetails.html',
-					controller: 'ScenarioInstanceController',
-					controllerAs: 'instanceCtrl'
-				}).
-			// Routes for Admin Dashbaord
-				when('/admin/jcomparser/', {
-					templateUrl: 'asset/templates/jcomparser.html',
-					controller: 'jcomparserMainView',
-					controllerAs: 'jcomparserMV'
-				}).
-				when('/admin/mail/', {
-					templateUrl: 'asset/templates/mailConfigDetails.html',
-					controller: 'mailConfig',
-					controllerAs: 'mailC'
-				}).
+        'ngRoute',
+        'adminConfiguration',
+        'userInteraction']);
+
+    // Defining Routes for the AngularJS App
+    jfrontend.config(['$routeProvider',
+        function ($routeProvider) {
+            $routeProvider.
+                // Routes for jCore
+                when('/scenario', {
+                    templateUrl: 'asset/templates/scenario.html',
+                    controller: 'ScenarioController',
+                    controllerAs: 'scenarioCtrl'
+                }).
+                when('/scenario/:id', {
+                    templateUrl: 'asset/templates/scenarioDetails.html',
+                    controller: 'ScenarioController',
+                    controllerAs: 'scenarioCtrl'
+                }).
+                when('/scenario/:id/instance', {
+                    templateUrl: 'asset/templates/scenarioInstance.html',
+                    controller: 'ScenarioInstanceController',
+                    controllerAs: 'instanceCtrl'
+                }).
+                when('/scenario/:id/instance/:instanceId', {
+                    templateUrl: 'asset/templates/scenarioInstanceDetails.html',
+                    controller: 'ScenarioInstanceController',
+                    controllerAs: 'instanceCtrl'
+                }).
+                // Routes for Admin Dashbaord
+                when('/admin/jcomparser/', {
+                    templateUrl: 'asset/templates/jcomparser.html',
+                    controller: 'jcomparserMainView',
+                    controllerAs: 'jcomparserMV'
+                }).
+                when('/admin/mail/', {
+                    templateUrl: 'asset/templates/mailConfigDetails.html',
+                    controller: 'mailConfig',
+                    controllerAs: 'mailC'
+                }).
                 when('/admin/webservice/', {
                     templateUrl: 'asset/templates/webserviceConfigDetails.html',
                     controller: 'webserviceConfig',
@@ -70,10 +70,10 @@ var JUserManagement_REST_Interface = "JUserManagement/api/interface/v1";
                     controller: 'userMgmtController',
                     controllerAs: 'userMgmtC'
                 }).
-			// default Route
-				otherwise({
-					redirectTo: '/scenario'
-				});
-		}
-	]);
+                // default Route
+                otherwise({
+                    redirectTo: '/scenario'
+                });
+        }
+    ]);
 })();
