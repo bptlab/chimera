f04fe0266fc1e93e1683d4ec81f678be16e9ecba
Jonas Beyer <jonas.beyer@student.hpi.de>
2016-01-14 15:16:17 +0100

    Implemented EventType and EventTypeAttribute similar to DataClass and DataAttribute

--- src/main/java/de/uni_potsdam/hpi/bpt/bp2014/jcomparser/xml/EventType.java
+++ src/main/java/de/uni_potsdam/hpi/bpt/bp2014/jcomparser/xml/EventType.java
@@ -1,7 +1,136 @@
 package de.uni_potsdam.hpi.bpt.bp2014.jcomparser.xml;
 
+import de.uni_potsdam.hpi.bpt.bp2014.jcomparser.Connector;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONArray;
+import org.w3c.dom.Node;
+
+import java.util.LinkedList;
+import java.util.List;
+
 /**
- * Created by jonas on 14.01.16.
+ * This class represents an EventType.
  */
-public class EventType {
+public class EventType implements IDeserialisableJson, IPersistable {
+    /**
+     * This is the modelID of the eventType.
+     */
+    private long eventTypeModelID;
+    /**
+     * This is the databaseID of the eventType.
+     */
+    private int eventTypeID;
+    /**
+     * This is the name of the eventType.
+     */
+    private String eventTypeName;
+    /**
+     * This is a list containing all eventTypeAttributes belonging to this eventType.
+     */
+    private List<EventTypeAttribute> eventTypeAttributes = new LinkedList<>();
+    /**
+     * This contains the JSON representation of the eventType.
+     */
+    private JSONObject eventTypeJson;
+
+    /**
+     * Sets the processeditorServerUrl which is needed for connecting to the server
+     * in order to get the XML-files for the fragments.
+     *
+     * @param serverURL This is the server URL.
+     */
+    public eventType(String serverURL) {
+
+    }
+
+    /**
+     * The standard constructor.
+     */
+    public eventType() {
+    }
+
+    /**
+     * This initializes the eventType from the JSON.
+     *
+     * @param element The JSON String which will be used for deserialisation
+     */
+    @Override public void initializeInstanceFromJson(final String element) {
+        try {
+            this.eventTypeJson = new JSONObject(element);
+
+            this.eventTypeName = this.eventTypeJson.getString("name");
+            this.eventTypeModelID = this.eventTypeJson.getLong("_id");
+            generateEventTypeAttributeList(this.eventTypeJson.getJSONArray("attributes"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override public void initializeInstanceFromXML(final Node element) {
+
+    }
+
+    /**
+     * This method saves the eventType to the database.
+     *
+     * @return the databaseID of the eventType.
+     */
+    @Override public int save() {
+        Connector conn = new Connector();
+        this.eventTypeID = conn.insertEventTypeIntoDatabase(this.eventTypeName);
+        saveEventTypeAttributes();
+
+        return eventTypeID;
+    }
+
+    /**
+     * This method gets all the eventTypeAttributes from the JSON.
+     * EventTypeAttributes can only be alphanumerical.
+     *
+     * @param jsonAttributes This JSONArray contains all eventTypeAttributes from the JSON.
+     */
+    private void generateEventTypeAttributeList(JSONArray jsonAttributes) {
+        int length = jsonAttributes.length();
+        for (int i = 0; i < length; i++) {
+            EventTypeAttribute eventTypeAttribute = new EventTypeAttribute(
+                    jsonAttributes.getJSONObject(i).getString("name"),
+                    jsonAttributes.getJSONObject(i).getString("datatype")
+            );
+            this.eventTypeAttributes.add(eventTypeAttribute);
+        }
+    }
+
+    /**
+     * This method iterates through all eventTypeAttributes and sets
+     * the eventType for them as well as calling the save method.
+     */
+    private void saveEventTypeAttributes() {
+        for (EventTypeAttribute eventTypeAttribute : this.eventTypeAttributes) {
+            eventTypeAttribute.setEventTypeID(eventTypeID);
+            eventTypeAttribute.save();
+        }
+    }
+
+    public int getEventTypeID() {
+        return eventTypeID;
+    }
+
+    public String getEventTypeName() {
+        return eventTypeName;
+    }
+
+    public List<EventTypeAttribute> getEventTypeAttributes() {
+        return eventTypeAttributes;
+    }
+
+
+    public JSONObject getEventTypeJson() {
+        return eventTypeJson;
+    }
+
+    public long getEventTypeModelID() {
+        return eventTypeModelID;
+    }
+
 }
--- src/main/java/de/uni_potsdam/hpi/bpt/bp2014/jcomparser/xml/EventTypeAttribute.java
+++ src/main/java/de/uni_potsdam/hpi/bpt/bp2014/jcomparser/xml/EventTypeAttribute.java
@@ -1,7 +1,86 @@
 package de.uni_potsdam.hpi.bpt.bp2014.jcomparser.xml;
 
+import de.uni_potsdam.hpi.bpt.bp2014.jcomparser.Connector;
+
 /**
- * Created by jonas on 14.01.16.
+ * This class represents a DataAttribute.
  */
-public class EventTypeAttribute {
+public class EventTypeAttribute implements IPersistable {
+    /**
+     * This is the databaseID of the eventType belonging to this attribute.
+     */
+    private int eventTypeID = -1;
+    /**
+     * This is the name of the attribute.
+     */
+    private String eventTypeAttributeName;
+    /**
+     * This is the type of the attribute.
+     */
+    private String eventTypeAttributeType;
+    /**
+     * This is the databaseID of the attribute.
+     */
+    private int eventTypeAttributeID;
+
+    /**
+     * Constructor which sets the Name of the eventTypeAttribute.
+     *
+     * @param name This is the name of the eventTypeAttribute.
+     */
+    public EventTypeAttribute(String name) {
+        this.eventTypeAttributeName = name;
+        this.eventTypeAttributeType = "";
+    }
+
+    /**
+     * Constructor which sets the Name and Type of the EventTypeAttribute.
+     *
+     * @param name This is the name of the EventTypeAttribute.
+     * @param type This is the dataype of the EventTypeAttribute.
+     */
+    public EventTypeAttribute(String name, String type) {
+        this.eventTypeAttributeName = name;
+        this.eventTypeAttributeType = type;
+    }
+
+    /**
+     * This constructor is only used for testCases
+     * therefore, a connection to the server is not needed.
+     */
+    public EventTypeAttribute() {
+    }
+
+    /**
+     * This method sets the databaseID of the eventType corresponding to the eventTypeAttribute.
+     *
+     * @param id This is the databaseID of the eventType
+     */
+    public void setEventTypeID(final int id) {
+        this.eventTypeID = id;
+    }
+
+    @Override public int save() {
+        Connector conn = new Connector();
+        this.eventTypeAttributeID = conn.insertEventTypeAttributeIntoDatabase(
+                this.eventTypeAttributeName, this.eventTypeID,
+                this.eventTypeAttributeType);
+        return eventTypeAttributeID;
+    }
+
+    public int getEventTypeID() {
+        return eventTypeID;
+    }
+
+    public int getEventTypeAttributeID() {
+        return eventTypeAttributeID;
+    }
+
+    public String getEventTypeAttributeType() {
+        return eventTypeAttributeType;
+    }
+
+    public String getEventTypeAttributeName() {
+        return eventTypeAttributeName;
+    }
 }
