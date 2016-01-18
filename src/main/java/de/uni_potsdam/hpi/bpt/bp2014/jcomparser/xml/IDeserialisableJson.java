ee44ea5aea62c92649d130c3fe0699789fabaf9d
PKuhn <patrick.kuhyn@student.hpi.de>
2016-01-18 01:49:23 +0100

    Removed support for xml initialization

--- src/main/java/de/uni_potsdam/hpi/bpt/bp2014/jcomparser/xml/IDeserialisableJson.java
+++ src/main/java/de/uni_potsdam/hpi/bpt/bp2014/jcomparser/xml/IDeserialisableJson.java
@@ -3,7 +3,7 @@ package de.uni_potsdam.hpi.bpt.bp2014.jcomparser.xml;
 import org.w3c.dom.*;
 
 /**
- * Created by Jonas on 03.12.2015.
+ *
  */
 public interface IDeserialisableJson {
 	/**
@@ -12,7 +12,4 @@ public interface IDeserialisableJson {
 	 * @param element The JSON Object which will be used for deserialisation
 	 */
 	void initializeInstanceFromJson(String element);
-
-	void initializeInstanceFromXML(org.w3c.dom.Node element);
-
 }
