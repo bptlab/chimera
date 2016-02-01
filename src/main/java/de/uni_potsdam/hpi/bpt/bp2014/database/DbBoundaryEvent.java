a37d0d6f1ce2ee1d301969517efe21b285af6574
PKuhn <patrick.kuhyn@student.hpi.de>
2016-02-01 09:18:57 +0100

    Improved code of DbBoundaryEvent and added Javadoc

--- src/main/java/de/uni_potsdam/hpi/bpt/bp2014/database/DbBoundaryEvent.java
+++ src/main/java/de/uni_potsdam/hpi/bpt/bp2014/database/DbBoundaryEvent.java
@@ -4,15 +4,23 @@ package de.uni_potsdam.hpi.bpt.bp2014.database;
  *
  */
 public class DbBoundaryEvent extends DbObject {
+    /**
+     *
+     * @param eventNodeId The control node Id of the boundary event
+     * @param fragmentInstanceId The id of the fragment instance where the bounday event is
+     *                           initialized in
+     * @return databaseId of the activity the bounday event is attached to
+     */
     public int getControlNodeAttachedToEvent(int eventNodeId, int fragmentInstanceId) {
-        String getModelId = "SELECT * FROM boundaryeventref where controlnode_id = "
+        String editorIdQuery = "SELECT * FROM boundaryeventref where controlnode_id = "
                 + eventNodeId + ";";
-        String modelId = this.executeStatementReturnsString(getModelId, "attachedtoref");
-        String getControlNodeForModelId = "SELECT * FROM controlnode WHERE modelid = "
-                + modelId + ";";
-        int controlNodeId = this.executeStatementReturnsInt(getControlNodeForModelId,
+        String editorId = this.executeStatementReturnsString(editorIdQuery, "attachedtoref");
+        String getControlNodeForEditorId = "SELECT * FROM controlnode WHERE modelid = "
+                + editorId + ";";
+        int activityControlNodeId = this.executeStatementReturnsInt(getControlNodeForEditorId,
                 "controlnode_id");
         DbControlNodeInstance controlNodeInstance = new DbControlNodeInstance();
-        return controlNodeInstance.getControlNodeInstanceID(controlNodeId, fragmentInstanceId);
+        return controlNodeInstance.getControlNodeInstanceID(activityControlNodeId,
+                fragmentInstanceId);
     }
 }
