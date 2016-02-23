9bdbf0bfa205e6eb9937830fcabac7d44dd016cf
PKuhn <patrick.kuhyn@student.hpi.de>
2016-02-23 13:51:18 +0100

    Added test for ensuring, that events enabled by an event based gateway get unregistered after triggering one of them

--- src/test/java/de/uni_potsdam/hpi/bpt/bp2014/jcore/EventGatewayTest.java
+++ src/test/java/de/uni_potsdam/hpi/bpt/bp2014/jcore/EventGatewayTest.java
@@ -2,9 +2,16 @@ package de.uni_potsdam.hpi.bpt.bp2014.jcore;
 
 import de.uni_potsdam.hpi.bpt.bp2014.AbstractDatabaseDependentTest;
 import de.uni_potsdam.hpi.bpt.bp2014.events.EventTestHelper;
+import de.uni_potsdam.hpi.bpt.bp2014.jcore.eventhandling.EventDispatcher;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Application;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
@@ -14,10 +21,22 @@ import static org.junit.Assert.assertEquals;
 /**
  *
  */
-public class EventGatewayTest {
+public class EventGatewayTest extends JerseyTest {
+    WebTarget base;
+
+    @Override
+    protected Application configure() {
+        return new ResourceConfig(EventDispatcher.class);
+    }
+
     @After
     public void teardown() throws IOException, SQLException {
         AbstractDatabaseDependentTest.resetDatabase();
+    }
+
+    @Before
+    public void setUpBase() {
+        base = target("eventdispatcher");
     }
 
     @Test
@@ -26,6 +45,7 @@ public class EventGatewayTest {
         try {
             ScenarioInstance scenarioInstance = EventTestHelper.createScenarioInstance(path);
             List<String> registeredEvents = scenarioInstance.getRegisteredEventKeys();
+
         } catch (IOException e) {
             assert(false);
             e.printStackTrace();
@@ -40,10 +60,24 @@ public class EventGatewayTest {
             ScenarioInstance scenarioInstance = EventTestHelper.createScenarioInstance(path);
             List<String> registeredEvents = scenarioInstance.getRegisteredEventKeys();
             assertEquals(3, registeredEvents.size());
+            triggerEventInScenario(scenarioInstance);
+            registeredEvents = scenarioInstance.getRegisteredEventKeys();
+            assertEquals(0, registeredEvents.size());
         } catch (IOException e) {
             assert(false);
             e.printStackTrace();
         }
 
     }
+
+    private void triggerEventInScenario(ScenarioInstance scenarioInstance) {
+        int scenarioId = scenarioInstance.getScenarioId();
+        int scenarioInstanceId = scenarioInstance.getScenarioInstanceId();
+        List<String> registeredEventKeys = scenarioInstance.getRegisteredEventKeys();
+        String registeredEvent = registeredEventKeys.get(0);
+        String route = String.format("scenario/%d/instance/%d/events/%s", scenarioId,
+                scenarioInstanceId, registeredEvent);
+        base.path(route).request().post(Entity.json(""));
+    }
+
 }
