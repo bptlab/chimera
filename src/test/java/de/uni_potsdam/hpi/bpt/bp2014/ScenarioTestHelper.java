b07cc75240fe4c193ce0fb979b77eb83418b9c09
PKuhn <patrick.kuhn@student.hpi.de>
2016-04-06 13:05:52 +0200

    Added test for exclusive gateways

--- src/test/java/de/uni_potsdam/hpi/bpt/bp2014/ScenarioTestHelper.java
+++ src/test/java/de/uni_potsdam/hpi/bpt/bp2014/ScenarioTestHelper.java
@@ -10,6 +10,7 @@ import javax.xml.bind.JAXBException;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * This class should be used to hold helper methods which are needed in tests
@@ -55,5 +56,34 @@ public class ScenarioTestHelper {
                 .findFirst().get();
 
         instance.terminate();
+    }
+
+    /**
+     *
+     */
+    public static boolean isActivityActivated(String activityName, ScenarioInstance scenarioInstance) {
+        Optional<AbstractControlNodeInstance> activity = findActivityInstanceByName(
+                activityName, scenarioInstance);
+        return activity.isPresent();
+    }
+
+    public static Optional<AbstractControlNodeInstance> findActivityInstanceByName(
+            String name, ScenarioInstance scenarioInstance) {
+        List<AbstractControlNodeInstance> controlNodeInstances =
+                scenarioInstance.getEnabledControlNodeInstances();
+
+        return controlNodeInstances.stream()
+                .filter(ActivityInstance.class::isInstance)
+                .filter(a -> ((ActivityInstance) a).getLabel().equals(name))
+                .findFirst();
+    }
+
+    public static void beginActivityByName(String name, ScenarioInstance scenarioInstance) {
+        Optional<AbstractControlNodeInstance> activity = findActivityInstanceByName(
+                name, scenarioInstance);
+        if (activity.isPresent()) {
+            ActivityInstance casted = (ActivityInstance) activity.get();
+            casted.begin();
+        }
     }
 }
