6a3fc4958b56fc8fd4c480e57f702c804a036f72
Jonas Beyer <jonas.beyer@student.hpi.de>
2016-07-18 13:17:41 +0200

    Remove unused config, log and samples folders

--- samples/FrontendMockup/v2/admin/pcm_image.php
+++
@@ -1,9 +0,0 @@
-<?php
-include '../core/config.php';
-include '../core/RESTCalls.php';
-
-$scenario_value = $_GET['id'];
-    $image= GetScenarioImage($scenario_value);
-    header('Content-type: image/png');
-    header('Content-Length: ' . filesize($image));
-    echo $image;
