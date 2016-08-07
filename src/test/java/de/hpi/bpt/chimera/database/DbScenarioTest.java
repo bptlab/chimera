5df7ae3e5e2223867a0684d8a6df7bc2a677f9ed
Jonas Beyer <jonas.beyer@student.hpi.de>
2016-08-07 20:28:43 +0200

    Rework DbScenarioTest

--- src/test/java/de/hpi/bpt/chimera/database/DbScenarioTest.java
+++ src/test/java/de/hpi/bpt/chimera/database/DbScenarioTest.java
@@ -1,28 +1,94 @@
 package de.hpi.bpt.chimera.database;
 
 import de.hpi.bpt.chimera.AbstractDatabaseDependentTest;
-import org.junit.Ignore;
+import de.hpi.bpt.chimera.jcomparser.saving.Connector;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
-/**
- *
- */
-public class DbScenarioTest extends AbstractDatabaseDependentTest {
-    /**
-     *
-     */
-    @Ignore @Test
-    public void testGetScenarioIDs() {
-        DbScenario dbScenario = new DbScenario();
-        List<Integer> scenarios = dbScenario.getScenarioIDs();
-        assertEquals(1, (int) scenarios.get(0));
-        assertEquals(2, (int) scenarios.get(1));
-        assertEquals(3, (int) scenarios.get(2));
-        assertEquals(100, (int) scenarios.get(3));
-        assertEquals(101, (int) scenarios.get(4));
+public class DbScenarioTest {
+
+    private static DbScenario dbScenario;
+    private static Connector connector;
+
+    private static int scenario1;
+    private static int scenario2;
+
+    @BeforeClass
+    public static void clearAndInitialize() throws IOException, SQLException {
+        dbScenario = new DbScenario();
+        connector = new Connector();
+        clearFragments();
+        insertTestData();
+    }
+
+    @AfterClass
+    public static void clearFragments() throws IOException, SQLException {
+        AbstractDatabaseDependentTest.resetDatabase();
+    }
+
+    private static void insertTestData() {
+        scenario1 = connector.insertScenario("scenario1", 1);
+        scenario2 = connector.insertScenario("scenario2", 1);
+    }
+
+    @Test
+    public void testGetScenarioIds() {
+        List<Integer> scenarios = dbScenario.getScenarioIds();
+        assertEquals(new Integer(scenario1), scenarios.get(0));
+        assertEquals(new Integer(scenario2), scenarios.get(1));
+    }
+
+    @Test
+    public void testExistScenario() {
+        assertTrue(dbScenario.existScenario(1));
+        assertTrue(dbScenario.existScenario(2));
+        assertFalse(dbScenario.existScenario(3));
+    }
+
+    @Test
+    public void testGetScenarioName(){
+        assertEquals("scenario1", dbScenario.getScenarioName(scenario1));
+        assertEquals("scenario2", dbScenario.getScenarioName(scenario2));
+    }
+
+    @Test
+    public void testGetScenarios(){
+        assertEquals(createScenarioMap(), dbScenario.getScenarios());
+    }
+
+    @Test
+    public void testGetScenariosLike(){
+        assertEquals(createScenarioMap(), dbScenario.getScenariosLike("scenario"));
+    }
+
+    @Test
+    public void testGetScenarioDetails(){
+        assertEquals(createScenarioDetailsMap(), dbScenario.getScenarioDetails(1));
+    }
+
+    private Map<Integer, String> createScenarioMap() {
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1, "scenario1");
+        map.put(2, "scenario2");
+        return map;
+    }
+
+    private Map<String, Object> createScenarioDetailsMap() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("id", 1);
+        map.put("name", "scenario1");
+        map.put("modelversion", 1);
+        return map;
     }
 }
