1ae426028f5fe7aa441a299b2de370dbb6807d41
Marcin Hewelt <marcin.hewelt@hpi.de>
2017-03-08 14:26:02 +0100

    Remove deprecated update scripts

--- execute_update_local.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mvn clean
-mvn install -Dmaven.test.skip
-service tomcat7 stop
-cp target/JEngine.war /var/lib/tomcat7/webapps/
-rm -r /var/lib/tomcat7/webapps/JEngine
-service tomcat7 start
-
