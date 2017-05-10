ab61f793b11ec9e310c314694ad2ae1853d26076
Jan Westphal <jan.westphal@student.hpi.de>
2017-05-10 14:33:09 +0200

    testing

--- src/main/java/de/hpi/bpt/chimera/model/datamodel/DataAttribute.java
+++ src/main/java/de/hpi/bpt/chimera/model/datamodel/DataAttribute.java
@@ -4,8 +4,19 @@ public class DataAttribute {
 	private String name;
 	private String type;
 
-	public DataAttribute(String name, String type) {
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
 		this.name = name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
 		this.type = type;
 	}
 }
