e6deb86d2f01b09164b47cfc3e09aa855d71a726
Finn K <finn-klessascheck@o2mail.de>
2017-06-18 22:10:51 +0200

    added upload function to angular controller and rest service
    still not sure if actuallry working or not

--- src/main/java/de/hpi/bpt/chimera/jcore/rest/ActivityRestService.java
+++ src/main/java/de/hpi/bpt/chimera/jcore/rest/ActivityRestService.java
@@ -11,12 +11,26 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import javax.ws.rs.*;
+import javax.ws.rs.core.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 import java.util.*;
 import java.util.stream.Collectors;
+//import javax.ws.rs.core.MultivaluedMap;//
+
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import org.glassfish.jersey.media.multipart.FormDataParam;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+//import org.apache.commons.io.IOUtils;
+import java.io.OutputStream;
+import javax.ws.rs.core.Response.ResponseBuilder;
+//import org.jboss.resteasy.plugins.providers.multipart.InputPart;
+//import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
 
 /**
  * This class implements the REST interface for activities.
@@ -418,4 +432,153 @@ public class ActivityRestService extends AbstractRestService {
 		}
 		return Response.status(Response.Status.ACCEPTED).type(MediaType.APPLICATION_JSON).entity("{\"message\":\"activity terminated.\"}").build();
 	}
-}
+
+	//FILE UPLOAD BELOW.
+
+	private static final String UPLOAD_FOLDER = "./uploadedFiles/";
+	@POST
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	@Path("files/{attributeID}/")
+	public Response uploadFile(
+			@FormDataParam("file") InputStream uploadedInputStream,
+			@FormDataParam("file") FormDataContentDisposition fileDetail, @PathParam("attributeID") String attributeID) {
+		// check if all form parameters are provided
+		
+		if (uploadedInputStream == null || fileDetail == null)
+			return Response.status(400).entity("Invalid form data").build();
+		// create destination folder, if it not exists
+		try {
+			createFolderIfNotExists(UPLOAD_FOLDER);
+		} catch (SecurityException se) {
+			return Response.status(500)
+					.entity("Can not create destination folder on server")
+					.build();
+		}
+		String uploadedFileLocation = UPLOAD_FOLDER + attributeID;
+		try {
+			saveToFile(uploadedInputStream, uploadedFileLocation);
+		} catch (IOException e) {
+			return Response.status(500).entity("Can not save file").build();
+		}
+		return Response.status(200)
+				.entity("File saved to " + uploadedFileLocation).build();
+	}
+	
+
+	/**
+	 * Utility method to save InputStream data to target location/file
+	 * 
+	 * @param inStream
+	 *            - InputStream to be saved
+	 * @param target
+	 *            - full path to destination file
+	 */
+	private void saveToFile(InputStream inStream, String target)
+			throws IOException {
+		OutputStream out = null;
+		int read = 0;
+		byte[] bytes = new byte[1024];
+		out = new FileOutputStream(new File(target));
+		while ((read = inStream.read(bytes)) != -1) {
+			out.write(bytes, 0, read);
+		}
+		out.flush();
+		out.close();
+	}
+	
+	
+	/**
+	 * Creates a folder to desired location if it not already exists
+	 * 
+	 * @param dirName
+	 *            - full path to the folder
+	 * @throws SecurityException
+	 *             - in case you don't have permission to create the folder
+	 */
+	 	private void createFolderIfNotExists(String dirName)
+			throws SecurityException {
+		File theDir = new File(dirName);
+		if (!theDir.exists()) {
+			theDir.mkdir();
+		}
+	}
+
+	
+
+	@GET
+	@Path("/file/{attributeID}")
+	@Produces(MediaType.APPLICATION_OCTET_STREAM)
+	public Response downloadFile(@PathParam("attributeID") String attributeID) {
+    	File file = new File("./FileUploads/"+ attributeID);
+    	ResponseBuilder response = Response.ok((Object) file);
+    	response.header("Content-Disposition", "attachment;filename=" +  attributeID);
+    	return response.build();
+	} 
+
+
+   /* @POST
+    @Path("file/{attributeID}/")
+    @Consumes("multipart/form-data")
+    public Response uploadFile(MultipartFormDataInput input, @PathParam("attributeID") String attributeID) throws IOException {
+          
+        Map<String, List<InputPart>> uploadForm = input.getFormDataMap();
+ 
+        // Get file data to save
+        List<InputPart> inputParts = uploadForm.get("attachment");
+ 
+        for (InputPart inputPart : inputParts) {
+            try {
+ 
+                MultivaluedMap<String, String> header = inputPart.getHeaders();
+                String fileName = getFileName(header);
+   
+                // convert the uploaded file to inputstream
+                InputStream inputStream = inputPart.getBody(InputStream.class,
+                        null);
+ 
+                byte[] bytes = IOUtils.toByteArray(inputStream);
+                // constructs upload file path
+                fileName = "./FileUploads/" + attributeID;
+                writeFile(bytes, fileName);
+ 
+                  
+                return Response.status(200).entity("Uploaded file name : " + fileName)
+                        .build();
+ 
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+ 
+    private String getFileName(MultivaluedMap<String, String> header) {
+ 
+        String[] contentDisposition = header.getFirst("Content-Disposition").split(";");
+ 
+        for (String filename : contentDisposition) {
+            if ((filename.trim().startsWith("filename"))) {
+ 
+                String[] name = filename.split("=");
+ 
+                String finalFileName = name[1].trim().replaceAll("\"", "");
+                return finalFileName;
+            }
+        }
+        return "unknown";
+    }
+ 
+    // Utility method that writes to file
+    private void writeFile(byte[] content, String filename) throws IOException {
+        File file = new File(filename);
+        if (!file.exists()) {
+            System.out.println("not exist> " + file.getAbsolutePath());
+            file.createNewFile();
+        }
+        FileOutputStream fop = new FileOutputStream(file);
+        fop.write(content);
+        fop.flush();
+        fop.close();
+    }*/
+}   
+
