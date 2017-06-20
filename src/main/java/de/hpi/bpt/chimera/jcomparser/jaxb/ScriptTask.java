9d5ae8384ac6df283283671696420789100083f2
Florian Marx <florian.marx@student.hpi.uni-potsdam.de>
2017-06-20 14:27:34 +0200

    first implementation of script task

---
+++ src/main/java/de/hpi/bpt/chimera/jcomparser/jaxb/ScriptTask.java
@@ -0,0 +1,7 @@
+package de.hpi.bpt.chimera.jcomparser.jaxb;
+
+/**
+ * Created by Florian on 20.06.2017.
+ */
+public class ScriptTask {
+}
---
+++ src/main/java/de/hpi/bpt/chimera/jcore/executionbehaviors/ScriptTaskExecutionBehavior.java
@@ -0,0 +1,7 @@
+package de.hpi.bpt.chimera.jcore.executionbehaviors;
+
+/**
+ * Created by Florian on 20.06.2017.
+ */
+public class ScriptTaskExecutionBehavior {
+}
