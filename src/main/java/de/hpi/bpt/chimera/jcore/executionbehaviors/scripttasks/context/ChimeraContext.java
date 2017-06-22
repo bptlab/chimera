51162d1c88e56f6001870675afd581a7d755f254
Marc André Freiheit <marcandre@freiheit.software>
2017-06-22 14:09:21 +0200

    Course structure of chimera context

--- src/main/java/de/hpi/bpt/chimera/jcore/executionbehaviors/scripttasks/context/ChimeraContext.java
+++ src/main/java/de/hpi/bpt/chimera/jcore/executionbehaviors/scripttasks/context/ChimeraContext.java
@@ -1,16 +1,43 @@
 package de.hpi.bpt.chimera.jcore.executionbehaviors.scripttasks.context;
 
+import com.sun.xml.bind.v2.schemagen.xmlschema.List;
+
+import de.hpi.bpt.chimera.jcore.controlnodes.ActivityInstance;
+
+/**
+ * Provide 
+ */
 public class ChimeraContext implements IChimeraContext {
+    private ActivityInstance activityInstance;
+
+    public void ChimeraContext(ActivityInstance activityInstance) {
+        this.activityInstance = activityInstance;
+    }
+
     public Object getParam(String name) {
         return null;
     }
     public void setParam(String name, Object value) {
 
     }
-    public int getProcessId() {
-        return 0;
+    public int getScenarioId() {
+        return activityInstance.getScenarioInstance().getId();
     }
+
+    /**
+     * Return the id of the activity itself.
+     */
     public int getActivityId() {
-        return 0;
+        return activityInstance.getControlNodeId();
+    }
+    /**
+     * Return the id of the fragment which owns this service task. 
+     */
+    public int getFragmentId() {
+        return activityInstance.getFragmentInstanceId();
+    }
+
+    public List<Object> getDataObjects() {
+        return activityInstance.getScenarioInstance().getDataManager().getDataObjectForId(0);
     }
 }
--- src/main/java/de/hpi/bpt/chimera/jcore/executionbehaviors/scripttasks/context/IChimeraContext.java
+++ src/main/java/de/hpi/bpt/chimera/jcore/executionbehaviors/scripttasks/context/IChimeraContext.java
@@ -3,6 +3,7 @@ package de.hpi.bpt.chimera.jcore.executionbehaviors.scripttasks.context;
 public interface IChimeraContext {
     Object getParam(String name);
     void setParam(String name, Object value);
-    int getProcessId();
+    int getScenarioId();
     int getActivityId();
+    int getFragmentId();
 }
