d551bd4d8a46c7c33957a0199d9131336d12e322
Florian Marx <florian.marx@student.hpi.uni-potsdam.de>
2017-07-04 13:56:11 +0200

    some code commentary

--- src/main/java/de/hpi/bpt/chimera/database/controlnodes/DbScriptTask.java
+++ src/main/java/de/hpi/bpt/chimera/database/controlnodes/DbScriptTask.java
@@ -2,16 +2,55 @@ package de.hpi.bpt.chimera.database.controlnodes;
 
 import de.hpi.bpt.chimera.database.DbObject;
 
+/**
+ * Represents the instance of a ScriptTask in the database.
+ * Used in {@link de.hpi.bpt.chimera.jcore.executionbehaviors.ScriptTaskExecutionBehavior} to retrieve
+ * data related to this instance.
+ */
 public class DbScriptTask extends DbObject {
 
+    /**
+     * Returns the relative path to the jar file which contains the script.
+     *
+     * @param controlNodeId The controlnode_id of the script task.
+     * @return a String with the relative path.
+     */
     public String getScriptTaskJar(int controlNodeId) {
         String sql = "SELECT scripttaskjar FROM scripttask " + "WHERE controlnode_id = " + controlNodeId;
         return this.executeStatementReturnsString(sql, "scripttaskjar");
     }
 
+    /**
+     * Updates the relative path to the jar file
+     *
+     * @param controlNodeId The controlnode_id of the script task.
+     * @param scripttaskjar The new relative path to the jar file
+     */
+    public void updateScriptTaskJar(int controlNodeId, String scripttaskjar) {
+        String sql = "UPDATE scripttask " + "SET scripttaskjar = '" + scripttaskjar + "' WHERE controlnode_id = " + controlNodeId;
+        executeUpdateStatement(sql);
+    }
+
+    /**
+     * Returns the classpath to the executable java file inside the jar.
+     *
+     * @param controlNodeId The controlnode_id of the script task.
+     * @return a String with the classpath.
+     */
     public String getScriptTaskClassPath(int controlNodeId) {
         String sql = "SELECT scripttaskclasspath FROM scripttask " + "WHERE controlnode_id = " + controlNodeId;
         return this.executeStatementReturnsString(sql, "scripttaskclasspath");
+    }
+
+    /**
+     * Updates the classpath for the given script task.
+     *
+     * @param controlNodeId         The controlnode_id of the script task.
+     * @param scripttaskclasspath   The new classpath to the executable java file inside the jar
+     */
+    public void updateScriptTaskClassPath(int controlNodeId, String scripttaskclasspath) {
+        String sql = "UPDATE scripttask " + "SET scripttaskclasspath = '" + scripttaskclasspath + "' WHERE controlnode_id = " + controlNodeId;
+        executeUpdateStatement(sql);
     }
 
 }
--- src/main/java/de/hpi/bpt/chimera/jcore/executionbehaviors/ScriptTaskExecutionBehavior.java
+++ src/main/java/de/hpi/bpt/chimera/jcore/executionbehaviors/ScriptTaskExecutionBehavior.java
@@ -1,28 +1,46 @@
 package de.hpi.bpt.chimera.jcore.executionbehaviors;
 
 import bpt.chimera.scripttasklibrary.IChimeraContext;
+import bpt.chimera.scripttasklibrary.IChimeraDelegate;
 import de.hpi.bpt.chimera.database.controlnodes.DbScriptTask;
 import de.hpi.bpt.chimera.jcore.controlnodes.ActivityInstance;
 import de.hpi.bpt.chimera.jcore.executionbehaviors.scripttasks.context.ChimeraContext;
 import org.apache.log4j.Logger;
 
 import java.io.File;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 
+/**
+ * Handles the script task execution behavior.
+ */
 public class ScriptTaskExecutionBehavior extends ActivityExecutionBehavior {
 
     private static Logger log = Logger.getLogger(ScriptTaskExecutionBehavior.class);
-    private final int controlNodeId;
 
+    /**
+     * Database ConnectionWrapper class for script tasks.
+     */
     private DbScriptTask dbScriptTask = new DbScriptTask();
 
-    public ScriptTaskExecutionBehavior(ActivityInstance instance) {
-        super(instance);
+    /**
+     * script task specific attributes
+     */
+    private final int controlNodeId;
+    private String scripttaskjar;
+    private String scripttaskclasspath;
+
+    public ScriptTaskExecutionBehavior(ActivityInstance activityInstance) {
+        super(activityInstance);
         controlNodeId = activityInstance.getControlNodeId();
     }
 
+    /**
+     * The handling method for script tasks execution behavior
+     */
     @Override
     public void execute() {
 
@@ -31,25 +49,76 @@ public class ScriptTaskExecutionBehavior extends ActivityExecutionBehavior {
         // TODO CHANGE
         //String path = System.getProperty("catalina.base") + "\\webapps\\Chimera-Resources\\" + dbScriptTask.getScriptTaskJar(controlNodeId);
 
-        String path = "D:\\Programme\\SimpleScriptTask.jar";
-        File file  = new File(path);
+        this.setValues();
 
-        URL url = null;
+        // get file object for jar file
+        File file;
         try {
-            url = file.toURL();
-            URL[] urls = new URL[]{url};
-
-            URLClassLoader cl = new URLClassLoader(urls, this.getClass().getClassLoader());
-
-            Class cls = Class.forName(dbScriptTask.getScriptTaskClassPath(controlNodeId), true, cl);
-            Object obj = cls.newInstance();
-
-            Method method = cls.getDeclaredMethod("execute", IChimeraContext.class);
-            method.invoke(obj, new ChimeraContext(activityInstance));
-        } catch (Exception e) {
-            e.printStackTrace();
+            file = new File(scripttaskjar);
+        } catch (NullPointerException e) {
+            log.error("The path to the jar file is not allowed to be 'null'!");
+            return;
         }
 
+        URL url;
+        try {
+            url = file.toURI().toURL();
+        } catch (MalformedURLException e) {
+            log.error("Could not get url from jar file, because it's malformed!");
+            return;
+        }
+
+        // build class loader for jar file
+        URL[] urls = new URL[]{url};
+        URLClassLoader cl = new URLClassLoader(urls, this.getClass().getClassLoader());
+
+        // get class and create new instance
+        Class delegateClass;
+        Object delegateInstance;
+        try {
+            delegateClass = Class.forName(scripttaskclasspath, true, cl);
+            delegateInstance = delegateClass.newInstance();
+        } catch (ClassNotFoundException e) {
+            log.error("Could not find class in jar! (" + scripttaskclasspath + ")");
+            return;
+        } catch (InstantiationException | IllegalAccessException e) {
+            log.error("An error has occurred by creating an instance of the java class! (" + e.getClass().getSimpleName() + ")");
+            return;
+        }
+
+        // check if class implements the 'IChimeraDelegate' interface
+        if(delegateInstance instanceof IChimeraDelegate) {
+            // get method object of 'execute' function
+            Method method;
+            try {
+                method = delegateClass.getDeclaredMethod("execute", IChimeraContext.class);
+            } catch (NoSuchMethodException e) {
+                log.error("This class doesn't contains the neccessary 'execute' method!");
+                return;
+            }
+
+            // invoke method on delegate instance
+            try {
+                method.invoke(delegateInstance, new ChimeraContext(activityInstance));
+            } catch (InvocationTargetException e) {
+                // TODO maybe we can add the name of the exception behind the text in the brackets
+                log.error("The class has thrown an error! ()");
+            } catch (IllegalAccessException e) {
+                log.error("The 'execute' method cannot be accessed!");
+            }
+        } else {
+            log.error("The class is not implementing from 'IChimeraDelegate'!");
+        }
+    }
+
+    /**
+     * Set the atrributes for the script task by reading from
+     * the database.
+     */
+    private void setValues() {
+        //scripttaskjar = dbScriptTask.getScriptTaskJar(controlNodeId);
+        scripttaskjar = "D:\\Programme\\SimpleScriptTask.jar";
+        scripttaskclasspath = dbScriptTask.getScriptTaskClassPath(controlNodeId);
     }
 
 }
