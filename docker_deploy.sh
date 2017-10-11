5606fe83e9edae813b083c92dc31f4c0f6e25904
m0r13 <moritz.hilscher@gmail.com>
2017-10-11 15:29:17 +0300

    Docker deploy script

---
+++ docker_deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#export CHIMERA_DEPLOY_NAME=chimera
+
+DEPLOY_PATH=/usr/local/tomcat/webapps/$CHIMERA_DEPLOY_NAME
+rm -rf $DEPLOY_PATH
+mkdir -p $DEPLOY_PATH
+cd $DEPLOY_PATH
+unzip /Chimera/Chimera.war > /dev/null
+
+envsubst < /Chimera/config.properties.tpl > $DEPLOY_PATH/WEB-INF/classes/config.properties
+
+catalina.sh run
