3af3734f8355c14c77a2faa01c98c9b3b450e931
Jan Westphal <jan.westphal306@gmail.com>
2018-06-26 01:42:59 +0200

    Fix dataflow parser test

--- src/test/java/de/hpi/bpt/chimera/parser/DataFlowParserTest.java
+++ src/test/java/de/hpi/bpt/chimera/parser/DataFlowParserTest.java
@@ -1,81 +1,53 @@
 package de.hpi.bpt.chimera.parser;
 
 import static org.junit.Assert.*;
-
+import static org.hamcrest.Matchers.is;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import de.hpi.bpt.chimera.CaseModelTestHelper;
 import de.hpi.bpt.chimera.model.CaseModel;
 import de.hpi.bpt.chimera.model.condition.ConditionSet;
+import de.hpi.bpt.chimera.model.condition.DataStateCondition;
 import de.hpi.bpt.chimera.model.fragment.bpmn.activity.AbstractActivity;
 
 public class DataFlowParserTest {
-	final String filepath = "src/test/resources/parser/JsonStringExtendedDataFlow";
+	final String filepath = "src/test/resources/parser/JsonStringExtendedDataFlow.json";
+
+	private List<String> expectedPrecondition = new ArrayList<>(Arrays.asList("A1 B1 C1", "A1 B1 C1", "A1 B2 C1", "A2 B1 C1", "A2 B1 C1", "A2 B2 C1"));
+	private List<String> expectedPostcondition = new ArrayList<>(Arrays.asList("A3 B3 C2", "A3 B4 C2", "A3 B5 C2", "A4 B3 C2", "A4 B4 C2", "A4 B5 C2"));
 
 	// TODO: make this independent from positions in list
-	@Test
-	public void parseCaseModel() {
 
-		// Parse a CaseModel from jsonString and give it to the CaseModel
-		// manager
+	private AbstractActivity task;
+
+	@Before
+	public void setup() {
 		CaseModel cm = CaseModelTestHelper.parseCaseModel(filepath);
-
-		AbstractActivity task = cm.getFragments().get(0).getBpmnFragment().getActivities().get(0);
+		task = cm.getFragments().get(0).getBpmnFragment().getActivities().get(0);
 		assertEquals(12, cm.getFragments().get(0).getBpmnFragment().getConditions().size());
-		List<ConditionSet> preCondition = task.getPreCondition().getConditionSets();
-		List<ConditionSet> postCondition = task.getPostCondition().getConditionSets();
+	}
 
-		// PreCondition
-		assertEquals("A1", preCondition.get(0).getConditions().get(0).getDataClassName() + preCondition.get(0).getConditions().get(0).getStateName());
-		assertEquals("B1", preCondition.get(0).getConditions().get(1).getDataClassName() + preCondition.get(0).getConditions().get(1).getStateName());
-		assertEquals("C1", preCondition.get(0).getConditions().get(2).getDataClassName() + preCondition.get(0).getConditions().get(2).getStateName());
+	private String conditionSetToString(ConditionSet cs) {
+		return cs.getConditions().stream().map(c -> String.format("%s%s ", c.getDataClassName(), c.getStateName())).sorted().collect(Collectors.joining()).trim();
+	}
 
-		assertEquals("A1", preCondition.get(1).getConditions().get(0).getDataClassName() + preCondition.get(1).getConditions().get(0).getStateName());
-		assertEquals("B2", preCondition.get(1).getConditions().get(1).getDataClassName() + preCondition.get(1).getConditions().get(1).getStateName());
-		assertEquals("C1", preCondition.get(1).getConditions().get(2).getDataClassName() + preCondition.get(1).getConditions().get(2).getStateName());
+	private List<String> conditionToString(DataStateCondition condition) {
+		return condition.getConditionSets().stream().map(this::conditionSetToString).sorted().collect(Collectors.toList());
+	}
 
-		assertEquals("A1", preCondition.get(2).getConditions().get(0).getDataClassName() + preCondition.get(2).getConditions().get(0).getStateName());
-		assertEquals("B1", preCondition.get(2).getConditions().get(1).getDataClassName() + preCondition.get(2).getConditions().get(1).getStateName());
-		assertEquals("C1", preCondition.get(2).getConditions().get(2).getDataClassName() + preCondition.get(2).getConditions().get(2).getStateName());
+	@Test
+	public void testPreConditionParsing() {
+		assertThat(expectedPrecondition, is(conditionToString(task.getPreCondition())));
+	}
 
-		assertEquals("A2", preCondition.get(3).getConditions().get(0).getDataClassName() + preCondition.get(3).getConditions().get(0).getStateName());
-		assertEquals("B1", preCondition.get(3).getConditions().get(1).getDataClassName() + preCondition.get(3).getConditions().get(1).getStateName());
-		assertEquals("C1", preCondition.get(3).getConditions().get(2).getDataClassName() + preCondition.get(3).getConditions().get(2).getStateName());
-
-		assertEquals("A2", preCondition.get(4).getConditions().get(0).getDataClassName() + preCondition.get(4).getConditions().get(0).getStateName());
-		assertEquals("B2", preCondition.get(4).getConditions().get(1).getDataClassName() + preCondition.get(4).getConditions().get(1).getStateName());
-		assertEquals("C1", preCondition.get(4).getConditions().get(2).getDataClassName() + preCondition.get(4).getConditions().get(2).getStateName());
-
-		assertEquals("A2", preCondition.get(5).getConditions().get(0).getDataClassName() + preCondition.get(5).getConditions().get(0).getStateName());
-		assertEquals("B1", preCondition.get(5).getConditions().get(1).getDataClassName() + preCondition.get(5).getConditions().get(1).getStateName());
-		assertEquals("C1", preCondition.get(5).getConditions().get(2).getDataClassName() + preCondition.get(5).getConditions().get(2).getStateName());
-
-		// PostCondition
-		assertEquals(6, postCondition.size());
-		assertEquals("A3", postCondition.get(0).getConditions().get(0).getDataClassName() + postCondition.get(0).getConditions().get(0).getStateName());
-		assertEquals("B3", postCondition.get(0).getConditions().get(1).getDataClassName() + postCondition.get(0).getConditions().get(1).getStateName());
-		assertEquals("C2", postCondition.get(0).getConditions().get(2).getDataClassName() + postCondition.get(0).getConditions().get(2).getStateName());
-
-		assertEquals("A3", postCondition.get(1).getConditions().get(0).getDataClassName() + postCondition.get(1).getConditions().get(0).getStateName());
-		assertEquals("B4", postCondition.get(1).getConditions().get(1).getDataClassName() + postCondition.get(1).getConditions().get(1).getStateName());
-		assertEquals("C2", postCondition.get(1).getConditions().get(2).getDataClassName() + postCondition.get(1).getConditions().get(2).getStateName());
-
-		assertEquals("A3", postCondition.get(2).getConditions().get(0).getDataClassName() + postCondition.get(2).getConditions().get(0).getStateName());
-		assertEquals("B5", postCondition.get(2).getConditions().get(1).getDataClassName() + postCondition.get(2).getConditions().get(1).getStateName());
-		assertEquals("C2", postCondition.get(2).getConditions().get(2).getDataClassName() + postCondition.get(2).getConditions().get(2).getStateName());
-
-		assertEquals("A4", postCondition.get(3).getConditions().get(0).getDataClassName() + postCondition.get(3).getConditions().get(0).getStateName());
-		assertEquals("B3", postCondition.get(3).getConditions().get(1).getDataClassName() + postCondition.get(3).getConditions().get(1).getStateName());
-		assertEquals("C2", postCondition.get(3).getConditions().get(2).getDataClassName() + postCondition.get(3).getConditions().get(2).getStateName());
-
-		assertEquals("A4", postCondition.get(4).getConditions().get(0).getDataClassName() + postCondition.get(4).getConditions().get(0).getStateName());
-		assertEquals("B4", postCondition.get(4).getConditions().get(1).getDataClassName() + postCondition.get(4).getConditions().get(1).getStateName());
-		assertEquals("C2", postCondition.get(4).getConditions().get(2).getDataClassName() + postCondition.get(4).getConditions().get(2).getStateName());
-
-		assertEquals("A4", postCondition.get(5).getConditions().get(0).getDataClassName() + postCondition.get(5).getConditions().get(0).getStateName());
-		assertEquals("B5", postCondition.get(5).getConditions().get(1).getDataClassName() + postCondition.get(5).getConditions().get(1).getStateName());
-		assertEquals("C2", postCondition.get(5).getConditions().get(2).getDataClassName() + postCondition.get(5).getConditions().get(2).getStateName());
+	@Test
+	public void testPostConditionParsing() {
+		assertThat(expectedPostcondition, is(conditionToString(task.getPostCondition())));
 	}
 }
