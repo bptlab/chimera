190066dde5286aff68d884507368790ed0e3086d
Marius Lichtblau <marius.lichtblau@googlemail.com>
2018-11-13 14:19:43 +0100

    Use Process Variable Notation
    Catch events filter incoming events with the #notation

--- src/main/java/de/hpi/bpt/chimera/execution/data/DataAttributeInstanceWriter.java
+++ src/main/java/de/hpi/bpt/chimera/execution/data/DataAttributeInstanceWriter.java
@@ -1,6 +1,8 @@
 package de.hpi.bpt.chimera.execution.data;
 
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.apache.log4j.Logger;
 
@@ -20,6 +22,15 @@ public final class DataAttributeInstanceWriter {
 	private DataAttributeInstanceWriter() {
 	}
 
+	public static void writeDataAttributeInstances(DataObject outputDataObject, Map<DataAttribute, String> dataAttributeToKeys, String json, List<DataObject> inputDataObjects) {
+		Map<DataAttribute, String> dataAttributeToJsonPath =
+				dataAttributeToKeys.entrySet().stream().filter(map -> map.getValue().startsWith("$")).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+		Map<DataAttribute, String> dataAttributeToProcessVariable =
+				dataAttributeToKeys.entrySet().stream().filter(map -> map.getValue().startsWith("#")).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+		DataAttributeInstanceWriter.writeDataAttributeInstancesFromJson(outputDataObject, dataAttributeToJsonPath, json);
+		DataAttributeInstanceWriter.writeDataAttributeInstancesFromDataObject(outputDataObject, dataAttributeToProcessVariable, inputDataObjects);
+	}
+
 	/**
 	 * Given a data object, some data represented as Json-String, and a map [attribute -> JsonPath expression]
 	 * this method will evaluate the JsonPath expression against the data and write the resulting values into 
@@ -29,7 +40,7 @@ public final class DataAttributeInstanceWriter {
 	 * @param dataAttributeToJsonPath - a map with a JsonPath expression for each attribute
 	 * @param json - the data as a Json-String
 	 */
-	public static void writeDataAttributeInstances(DataObject dataObject, Map<DataAttribute, String> dataAttributeToJsonPath, String json) {
+	public static void writeDataAttributeInstancesFromJson(DataObject dataObject, Map<DataAttribute, String> dataAttributeToJsonPath, String json) {
 		log.info(String.format("Writing attributes for DO <%s> based on data%n%s", dataObject, json));
 		if (dataAttributeToJsonPath == null) {
 			log.error("No map [Attribute -> JsonPath Expression] provided");
@@ -48,6 +59,39 @@ public final class DataAttributeInstanceWriter {
 				}
 			} catch (Exception e) {
 				log.error("Error while evaluating JsonPath expression against Json data. Check the JsonPath expression and data.", e);
+				dataAttributeInstance.setValue("ERROR");
+			}
+		}
+	}
+
+	public static void writeDataAttributeInstancesFromDataObject(DataObject outputDataObject, Map<DataAttribute, String> dataAttributeToProcessVariable, List<DataObject> inputDataObjects) {
+		log.info(String.format("Writing attributes for DO <%s> based on data...", outputDataObject));
+		if (dataAttributeToProcessVariable == null) {
+			log.error("No map [Attribute -> Process Variable Expression] provided");
+			return;
+		}
+		for (DataAttributeInstance dataAttributeInstance : outputDataObject.getDataAttributeInstances()) {
+
+			if (!dataAttributeToProcessVariable.containsKey(dataAttributeInstance.getDataAttribute())) {
+				continue;
+			}
+			String processVariable = dataAttributeToProcessVariable.get(dataAttributeInstance.getDataAttribute());
+			try {
+				if (processVariable != null && !processVariable.isEmpty()) {
+					processVariable = processVariable.substring(1);
+					String dcReference = processVariable.split("\\.")[0];
+					String attrReference = processVariable.split("\\.")[1];
+					DataObject inputDataObject =
+							inputDataObjects
+									.stream()
+									.filter(x -> x.getDataClass().getName() == dcReference)
+									.findFirst()
+									.get();
+					String value = inputDataObject.getDataAttributeInstanceByName(attrReference).getValue().toString();
+					dataAttributeInstance.setValue(value);
+				}
+			} catch (Exception e) {
+				log.error("Error while evaluating ProcessVariable expression against Data Object. Check the Process Variable and data.", e);
 				dataAttributeInstance.setValue("ERROR");
 			}
 		}
